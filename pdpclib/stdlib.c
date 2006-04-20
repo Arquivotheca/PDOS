a4ddf3072c8b4d61d4d84a348979eb9b35409976
kerravon86 <kerravon86>
2006-04-20 02:14:46 +0000

    more changes from Dave Wade

--- pdpclib/stdlib.c
+++ pdpclib/stdlib.c
@@ -344,42 +344,92 @@ double atof(const char *nptr)
 double strtod(const char *nptr, char **endptr)
 {
     double x = 0.0;
+    double xs= 1.0;
+    double es = 1.0;
     double xf = 0.0;
     double xd = 1.0;
+    int dp = 0;
+
+
+ /* while(isspace(*nptr))++nptr; */
+    if(*nptr == '-')
+    {
+        xs = -1;
+        nptr++;
+    }
 
     while (1)
     {
         if (isdigit(*nptr))
         {
             x = x * 10 + (*nptr - '0');
-        }
-        else if ( *nptr == '.' )
-        {
-            nptr ++;
-            while (1)
-            {
-                if (isdigit(*nptr))
-                {
-                    xf = xf * 10 + (*nptr - '0');
-                    xd = xd * 10;
-                }
-                else
-                {
-                    x = x + xf/xd;
-                    break;
-                }
-                nptr++;
-            }
+            nptr++;
         }
         else
         {
-            if (endptr != NULL)
-            {
-                *endptr = (char *)nptr;
-            }
+            x = x * xs;
             break;
         }
-        nptr++;
+    }
+    if ( *nptr == '.' )
+    {
+        dp=1;
+        nptr ++;
+        while (1)
+        {
+            if (isdigit(*nptr))
+            {
+                xf = xf * 10 + (*nptr - '0');
+                xd = xd * 10;
+            }
+            else
+            {
+                x = x + xs*(xf/xd);
+                break;
+            }
+            nptr++;
+        }
+    }
+    if ( (*nptr == 'e') | (*nptr == 'E') )
+    {
+        nptr ++;
+        if(*nptr == '-')
+        {
+            es = -1;
+            nptr++;
+        }
+        dp=0;
+        xd = 1;
+        xf = 0;
+        while (1)
+        {
+            if (isdigit(*nptr))
+            {
+                xf = xf * 10 + (*nptr - '0');
+                nptr++;
+            }
+            else
+            {
+                while (xf > 0)
+                {
+                    xd *=10;
+                    xf--;
+                }
+                if ( es < 0.0 )
+                {
+                    x=x/xd;
+                }
+                else
+                {
+                    x=x*xd;
+                }
+                break;
+            }
+        }
+    }
+    if (endptr != NULL)
+    {
+        *endptr = (char *)nptr;
     }
     return (x);
 }

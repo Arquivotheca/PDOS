146dc1e7b4aa30e51c2caaa1a602a82cb57c5c86
kerravon86 <kerravon86>
2006-11-16 03:56:45 +0000

    make pi and ln10 static

--- pdpclib/math.c
+++ pdpclib/math.c
@@ -51,8 +51,8 @@
   (These should I guess be in math.h)
 
 */
-const double pi   = 3.1415926535897932384626433832795;
-const double ln10 = 2.3025850929940456840179914546844;
+static const double pi   = 3.1415926535897932384626433832795;
+static const double ln10 = 2.3025850929940456840179914546844;
 
 
 double ceil(double x)

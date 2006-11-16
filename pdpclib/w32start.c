4a8c1ce08b413720d065da60d0fddfa246931dd0
kerravon86 <kerravon86>
2006-11-16 11:57:12 +0000

    win32 startup code

---
+++ pdpclib/w32start.c
@@ -0,0 +1,24 @@
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  w32start - startup code for WIN32                                */
+/*                                                                   */
+/*********************************************************************/
+
+/* This is the main entry point of a console mode executable */
+
+void mainCRTStartup(void)
+{
+    __start(0);
+    return;
+}
+
+void __main(void)
+{
+    return;
+}

51a8b9bf1ef9a00bdfff1460fed1e49eb62544ca
kerravon86 <kerravon86>
2006-11-30 22:35:42 +0000

    move handlers from lldos to handlers

--- src/comp2.bat
+++ src/comp2.bat
@@ -1,9 +1,10 @@
 tasm -Dmemodel=large pdosstrt.asm
 tasm -Dmemodel=large support.asm
-tasm -Dmemodel=large -DNEED_HANDLER lldos.asm
+tasm -Dmemodel=large lldos.asm
+tasm -Dmemodel=large handlers.asm
 tasm -Dmemodel=large ..\pdpclib\dossupa.asm
 tcc -O -c -ml -I..\pdpclib memmgr.c format.c patmat.c
 tcc -O -c -ml -I..\pdpclib bos.c fat.c ..\pdpclib\string.c ..\pdpclib\ctype.c ..\pdpclib\dossupc.c
 rem bcc -w- to switch off warnings
 tcc -O -j1 -c -ml -I..\pdpclib pdos.c 
-tlink -x pdosstrt+pdos+bos+support+fat+dossupa+string+dossupc+lldos+memmgr+format+ctype+patmat,pdos.exe
+tlink -x pdosstrt+pdos+bos+support+fat+dossupa+string+dossupc+lldos+handlers+memmgr+format+ctype+patmat,pdos.exe

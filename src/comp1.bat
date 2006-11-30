9d26a27510b406d362cf68b8c67e001aa2191eda
kerravon86 <kerravon86>
2006-11-30 05:30:52 +0000

    no need for special options on tasm

--- src/comp1.bat
+++ src/comp1.bat
@@ -1,11 +1,11 @@
 @echo off
-tasm -zi -mx -Dmemodel=tiny ploadst.asm
-tasm -zi -mx -Dmemodel=tiny int13x.asm
-tasm -zi -mx -Dmemodel=tiny near.asm
+tasm -Dmemodel=tiny ploadst.asm
+tasm -Dmemodel=tiny int13x.asm
+tasm -Dmemodel=tiny near.asm
 
 rem these two files are shared between modules
-tasm -zi -mx -Dmemodel=tiny support.asm 
-tasm -zi -mx -Dmemodel=tiny lldos.asm
+tasm -Dmemodel=tiny support.asm 
+tasm -Dmemodel=tiny lldos.asm
 tcc -O -c -mt -DNEED_DUMP -I..\pdpclib pload.c fat.c bos.c pdosload.c ..\pdpclib\string.c ..\pdpclib\dossupc.c
 rem the order of the following stuff is critical so that the first 3*512
 rem bytes have sufficient stuff to do the rest of the load of itself!!!
--- src/comp4.bat
+++ src/comp4.bat
@@ -1,10 +1,10 @@
 @echo off
-tasm -zi -mx -Dmemodel=tiny support.asm 
-tasm -zi -mx -Dmemodel=tiny ploadst.asm
-tasm -zi -mx -Dmemodel=tiny near.asm
-tasm -zi -mx -Dmemodel=tiny lldos.asm
-tasm -zi -mx -Dmemodel=tiny protinta.asm
-tasm -zi -mx -Dmemodel=tiny int13x.asm
+tasm -Dmemodel=tiny support.asm 
+tasm -Dmemodel=tiny ploadst.asm
+tasm -Dmemodel=tiny near.asm
+tasm -Dmemodel=tiny lldos.asm
+tasm -Dmemodel=tiny protinta.asm
+tasm -Dmemodel=tiny int13x.asm
 tcc -O -c -mt -DPDOS32 -I..\pdpclib pload.c protint.c file.c
 tcc -O -c -mt -DNEED_DUMP -DPDOS32 -I..\pdpclib fat.c bos.c pdosload.c ..\pdpclib\string.c ..\pdpclib\dossupc.c
 rem the order of the following stuff is critical so that the first 3*512

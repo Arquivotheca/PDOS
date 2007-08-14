4bf378c2e137c92437ec2c782c9958425dc9ec02
kerravon86 <kerravon86>
2007-08-14 11:01:03 +0000

    better way to compile world32

--- src/compw32.bat
+++ src/compw32.bat
@@ -1,4 +1,2 @@
 gcc -s -c -I../pdpclib world.c
-ld -s -o world ../pdpclib/pdosst32.o world.o ../pdpclib/pdos.a
-if exist world.exe del world.exe
-ren world world.exe
+gcc -nostdinc -nostdlib -s -o world.exe ../pdpclib/pdosst32.o world.o ../pdpclib/pdos.a

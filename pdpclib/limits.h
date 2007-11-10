25fd709e71c7e9210d0391028312530a87252ad9
kerravon86 <kerravon86>
2007-11-10 11:31:23 +0000

    fix limits to eliminate warnings

--- pdpclib/limits.h
+++ pdpclib/limits.h
@@ -25,25 +25,25 @@
 #define CHAR_MAX 255
 #endif
 #define MB_LEN_MAX 1
-#define SHRT_MIN -32768
+#define SHRT_MIN (-SHRT_MAX-1)
 #define SHRT_MAX 32767
-#define USHRT_MAX 65535
+#define USHRT_MAX ((unsigned short)65535U)
 
 #if (defined(__OS2__) || defined(__32BIT__) || defined(__MVS__) \
     || defined(__CMS__) || defined(__WIN32__))
-#define INT_MIN -2147483648
+#define INT_MIN (-INT_MAX-1)
 #define INT_MAX 2147483647
 #define UINT_MAX 4294967295U
 #endif
 
 #if (defined(__MSDOS__) || defined(__DOS__) || defined(__POWERC))
-#define INT_MIN -32768
+#define INT_MIN (-INT_MAX-1)
 #define INT_MAX 32767
-#define UINT_MAX 65535
+#define UINT_MAX 65535U
 #endif
 
-#define LONG_MIN -2147483648
-#define LONG_MAX 2147483647
-#define ULONG_MAX 4294967295U
+#define LONG_MIN (-LONG_MAX-1)
+#define LONG_MAX 2147483647L
+#define ULONG_MAX 4294967295UL
 
 #endif

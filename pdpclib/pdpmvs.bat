eb5b2ebeb27306cb4501c70b897aab6456856eaa
kerravon86 <kerravon86>
2007-11-20 00:22:07 +0000

    used to test pdpclib on mvs

---
+++ pdpclib/pdpmvs.bat
@@ -0,0 +1,14 @@
+gccmvs -S -I . start.c
+gccmvs -S -I . stdio.c
+gccmvs -S -I . stdlib.c
+gccmvs -S -I . ctype.c
+gccmvs -S -I . string.c
+gccmvs -S -I . time.c
+gccmvs -S -I . errno.c
+gccmvs -S -I . assert.c
+gccmvs -S -I . locale.c
+gccmvs -S -I . math.c
+gccmvs -S -I . setjmp.c
+gccmvs -S -I . signal.c
+gccmvs -S -I . pdptest.c
+m4 -I . pdpmvs.m4 >pdpmvs.jcl

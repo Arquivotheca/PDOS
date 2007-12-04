b5e5c1ed10dde018abbc48d0164677976e332987
kerravon86 <kerravon86>
2007-12-04 07:24:50 +0000

    new script name

--- pdpclib/pdpmvs.bat
+++ pdpclib/pdpmvs.bat
@@ -14,4 +14,4 @@ gccmvs -DUSE_MEMMGR -S -I . __memmgr.c
 gccmvs -DUSE_MEMMGR -S -I . pdptest.c
 m4 -I . pdpmvs.m4 >pdpmvs.jcl
 rem call sub pdpmvs.jcl
-call runmvs pdpmvs.jcl output.txt
+call runmvsp pdpmvs.jcl output.txt

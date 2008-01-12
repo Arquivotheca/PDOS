60490d2e9b64ecd5ff89c8c8f312bc1b600c97aa
kerravon86 <kerravon86>
2008-01-12 03:00:00 +0000

    add debugging facility and fix bug

--- pdpclib/__memmgr.h
+++ pdpclib/__memmgr.h
@@ -10,7 +10,16 @@
 
 #include <stddef.h>
 
-/* #define __MEMMGR_INTEGRITY 1 */
+/* Do you want memmgr to perform lots of integrity checks? */
+/* Note that this will slow down the system, but it doesn't
+   print out anything or change the functionality of your
+   application. */
+#define __MEMMGR_INTEGRITY 1
+
+/* Do you want lots of debugging output? */
+/* Note that you still need to set the memmgrDebug variable to 1
+   before it is actually activated */
+#define __MEMMGR_DEBUGGING 1
 
 typedef struct memmgrn {
 #ifdef __MEMMGR_INTEGRITY
@@ -48,8 +57,12 @@ typedef struct {
 #define MEMMGR_MINFREE_INTERNAL 256
 
 /* minimum size we will give to an application for its data */
-#define MEMMGR_MINFREE (MEMMGRN_SZ < MEMMGR_MINFREE_INTERNAL ? \
+#define MEMMGR_MINFREE_INT2 (MEMMGRN_SZ < MEMMGR_MINFREE_INTERNAL ? \
     MEMMGR_MINFREE_INTERNAL - MEMMGRN_SZ : MEMMGRN_SZ)
+
+#define MEMMGR_MINFREE ((MEMMGR_MINFREE_INT2 % MEMMGR_ALIGN == 0) ? \
+    MEMMGR_MINFREE_INT2 : ((MEMMGR_MINFREE_INT2 / MEMMGR_ALIGN) + 1) \
+                           * MEMMGR_ALIGN)
 
 /* do you want to crash whenever an integrity problem arises? */
 #ifndef MEMMGR_CRASH
@@ -66,6 +79,7 @@ typedef struct {
 #define memmgrMaxSize __mmMaxSize
 #define memmgrIntegrity __mmIntegrity
 #define memmgrRealloc __mmRealloc
+#define memmgrDebug __mmDebug
 
 void memmgrDefaults(MEMMGR *memmgr);
 void memmgrInit(MEMMGR *memmgr);
@@ -77,6 +91,8 @@ void memmgrFreeId(MEMMGR *memmgr, int id);
 size_t memmgrMaxSize(MEMMGR *memmgr);
 void memmgrIntegrity(MEMMGR *memmgr);
 int memmgrRealloc(MEMMGR *memmgr, void *ptr, size_t newsize);
+
+extern int memmgrDebug;
 
 extern MEMMGR __memmgr;
 

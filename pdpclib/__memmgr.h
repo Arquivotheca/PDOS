860a790991f32bdd27e7e0ed0c9d8909e225ed05
kerravon86 <kerravon86>
2008-01-13 08:37:14 +0000

    extra debug variable, switch off debugging

--- pdpclib/__memmgr.h
+++ pdpclib/__memmgr.h
@@ -14,12 +14,12 @@
 /* Note that this will slow down the system, but it doesn't
    print out anything or change the functionality of your
    application. */
-#define __MEMMGR_INTEGRITY 1
+/* #define __MEMMGR_INTEGRITY 1 */
 
 /* Do you want lots of debugging output? */
 /* Note that you still need to set the memmgrDebug variable to 1
    before it is actually activated */
-#define __MEMMGR_DEBUG 1
+/* #define __MEMMGR_DEBUG 1 */
 
 typedef struct memmgrn {
 #ifdef __MEMMGR_INTEGRITY
@@ -80,6 +80,7 @@ typedef struct {
 #define memmgrIntegrity __mmIntegrity
 #define memmgrRealloc __mmRealloc
 #define memmgrDebug __mmDebug
+#define memmgrDebug2 __mmDbg2
 
 void memmgrDefaults(MEMMGR *memmgr);
 void memmgrInit(MEMMGR *memmgr);
@@ -93,6 +94,7 @@ void memmgrIntegrity(MEMMGR *memmgr);
 int memmgrRealloc(MEMMGR *memmgr, void *ptr, size_t newsize);
 
 extern int memmgrDebug;
+extern int memmgrDebug2;
 
 extern MEMMGR __memmgr;
 

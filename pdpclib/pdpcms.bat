6d4829c3bda5d8ab727e8f28fb70c29ddb7b9121
kerravon86 <kerravon86>
2008-10-04 21:20:15 +0000

    support z/VM

--- pdpclib/pdpcms.bat
+++ pdpclib/pdpcms.bat
@@ -13,4 +13,10 @@ gcccms -DXXX_MEMMGR -S -I . setjmp.c
 gcccms -DXXX_MEMMGR -S -I . signal.c
 gcccms -DXXX_MEMMGR -S -I . pdptest.c
 zip -0X pdpcms *.s *.exec *.asm *.mac
+
+rem Useful for VM/380
 call runcms pdpcms.exec output.txt pdpcms.zip
+
+rem Useful for z/VM
+rem mvsendec encb pdpcms.zip pdpcms.dat
+rem loc2ebc pdpcms.dat transfer.card 80

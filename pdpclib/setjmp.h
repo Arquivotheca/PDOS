02d31e0a3d61bde6d9d39e1a58cf6928fa4fc6eb
kerravon86 <kerravon86>
2009-03-30 12:05:29 +0000

    support setjmp for Win32

--- pdpclib/setjmp.h
+++ pdpclib/setjmp.h
@@ -15,21 +15,32 @@
 
 typedef struct {
 #if defined(__MVS__) || defined(__CMS__)
+
     int saveptr;   /* pointer to stack savearea */
     int savelng;  /* length of save area */
     int savestk;  /* where to put it */
     int saver13; /* Where to leave it pointing to */
     int saver14; /* and return address */
-#else
-    int eax;
+
+#elif defined(__WIN32__)
     int ebx;
     int ecx;
+    int edx;
+    int edi;
+    int esi;
+    int esp;
+    int ebp;
+    int retaddr;
 #endif
-    int longj;
-    int ret;
+    int retval;
 } jmp_buf[1];
 
+#if defined(__WIN32__)
+int __setj(void *);
+#define setjmp(x) (__setj(x))
+#else
 int setjmp(jmp_buf env);
+#endif
 void longjmp(jmp_buf env, int val);
 
 #endif

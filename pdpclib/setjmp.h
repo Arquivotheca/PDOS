c74302316e5603fc10b82a4dded016468c857d5d
kerravon86 <kerravon86>
2009-03-31 11:55:17 +0000

    add setjmp/longjmp support for dos, os/2 and pdos

--- pdpclib/setjmp.h
+++ pdpclib/setjmp.h
@@ -22,7 +22,7 @@ typedef struct {
     int saver13; /* Where to leave it pointing to */
     int saver14; /* and return address */
 
-#elif defined(__WIN32__)
+#elif defined(__WIN32__) || defined(__32BIT__) || defined(__OS2__)
     int ebx;
     int ecx;
     int edx;
@@ -31,12 +31,26 @@ typedef struct {
     int esp;
     int ebp;
     int retaddr;
+
+#elif defined(__MSDOS__) || defined(__DOS__) || defined(__POWERC)
+    int bx;
+    int cx;
+    int dx;
+    int di;
+    int si;
+    int sp;
+    int bp;
+    void *retaddr;
+    int ds;
+    int es;
 #endif
+
     int retval;
 } jmp_buf[1];
 
-#if defined(__WIN32__)
-int __setj(void *);
+#if defined(__WIN32__) || defined(__MSDOS__) || defined(__DOS__) \
+  || defined(__POWERC) || defined(__OS2__) || defined(__32BIT__)
+int __setj(jmp_buf env);
 #define setjmp(x) (__setj(x))
 #else
 int setjmp(jmp_buf env);

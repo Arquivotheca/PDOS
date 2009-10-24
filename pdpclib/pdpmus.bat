d17517f6aebe3b4e36dab20c6ebc841abf3d5e1f
kerravon86 <kerravon86>
2009-10-24 13:42:51 +0000

    use memmgr by default

--- pdpclib/pdpmus.bat
+++ pdpclib/pdpmus.bat
@@ -3,20 +3,20 @@ rem also choose between S/380 and S/370. If choosing S/370,
 rem you should not use MEMMGR (unless you adjust the
 rem constants in stdlib.c). If using S/380 you must use MEMMGR.
 
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . start.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . stdio.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . stdlib.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . ctype.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . string.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . time.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . errno.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . assert.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . locale.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . math.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . setjmp.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . signal.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . __memmgr.c
-gccmvs -DLOCMODE -DXXX_MEMMGR -S -I . pdptest.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . start.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . stdio.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . stdlib.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . ctype.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . string.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . time.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . errno.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . assert.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . locale.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . math.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . setjmp.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . signal.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . __memmgr.c
+gccmvs -DLOCMODE -DUSE_MEMMGR -S -I . pdptest.c
 m4 -I . pdpmus.m4 >pdpmus.job
 rem call sub pdpmus.job
 call runmus pdpmus.job output.txt

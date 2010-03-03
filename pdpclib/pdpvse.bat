90f3dece629302b457d11875f56137a5f552ebe3
kerravon86 <kerravon86>
2010-03-03 10:39:05 +0000

    working DOS/VS

--- pdpclib/pdpvse.bat
+++ pdpclib/pdpvse.bat
@@ -1,16 +1,20 @@
 del pdpvse.zip
-gcccms -D__VSE__ -S -I . start.c
-gcccms -D__VSE__ -S -I . stdio.c
-gcccms -D__VSE__ -S -I . stdlib.c
-gcccms -D__VSE__ -S -I . ctype.c
-gcccms -D__VSE__ -S -I . string.c
-gcccms -D__VSE__ -S -I . time.c
-gcccms -D__VSE__ -S -I . errno.c
-gcccms -D__VSE__ -S -I . assert.c
-gcccms -D__VSE__ -S -I . locale.c
-gcccms -D__VSE__ -S -I . math.c
-gcccms -D__VSE__ -S -I . setjmp.c
-gcccms -D__VSE__ -S -I . signal.c
-gcccms -D__VSE__ -S -I . __memmgr.c
-gcccms -D__VSE__ -S -I . pdptest.c
-zip -0X pdpvse *.s *.exec *.asm *.mac
+gccmvs -Os -D__VSE__ -S -I . -o - start.c | namcsect START >start.s
+gccmvs -Os -D__VSE__ -S -I . -o - stdio.c | namcsect @@STDIO >stdio.s
+gccmvs -Os -D__VSE__ -S -I . -o - stdlib.c | namcsect @@STDLIB >stdlib.s
+gccmvs -Os -D__VSE__ -S -I . -o - ctype.c | namcsect @@CTYPE >ctype.s
+gccmvs -Os -D__VSE__ -S -I . -o - string.c | namcsect @@STRING >string.s
+gccmvs -Os -D__VSE__ -S -I . -o - time.c | namcsect @@PDPTIM >time.s
+gccmvs -Os -D__VSE__ -S -I . -o - errno.c | namcsect @@PDPER >errno.s
+gccmvs -Os -D__VSE__ -S -I . -o - assert.c | namcsect @@PDPASS >assert.s
+gccmvs -Os -D__VSE__ -S -I . -o - locale.c | namcsect @@PDPLOC >locale.s
+gccmvs -Os -D__VSE__ -S -I . -o - math.c | namcsect @@MATH >math.s
+gccmvs -Os -D__VSE__ -S -I . -o - setjmp.c | namcsect @@PDPJMP >setjmp.s
+gccmvs -Os -D__VSE__ -S -I . -o - signal.c | namcsect @@PDPSIG >signal.s
+gccmvs -Os -D__VSE__ -S -I . -o - __memmgr.c | namcsect @@PDPMEM >__memmgr.s
+gccmvs -Os -D__VSE__ -S -I . -o - pdptest.c | namcsect PDPTEST >pdptest.s
+rem zip -0X pdpvse *.s *.exec *.asm *.mac
+
+m4 -I . pdpvse.m4 >pdpvse.jcl
+rem call sub pdpvse.jcl
+call runvse pdpvse.jcl output.txt

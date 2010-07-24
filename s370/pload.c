d51e8029b5ed2a3c2cd267d8c9b197ff4899549a
kerravon86 <kerravon86>
2010-07-24 08:18:59 +0000

    start calling initsys

--- s370/pload.c
+++ s370/pload.c
@@ -8,6 +8,18 @@
 /*                                                                   */
 /*  pload.c - Load PDOS/370                                          */
 /*                                                                   */
+/*  When this (main) program is entered, interrupts are disabled,    */
+/*  and this program needs to find the PDOS program and load it into */
+/*  memory, and transfer control, again with interrupts disabled.    */
+/*                                                                   */
+/*  The way it does this is that when doing an I/O, it only          */
+/*  enables the I/O then and there, and waits on it to complete,     */
+/*  at which point it is disabled again.                             */
+/*                                                                   */
+/*  The operating system itself won't be able to get away with this  */
+/*  simplistic logic because it needs to deal with timer interrupts  */
+/*  etc.                                                             */
+/*                                                                   */
 /*********************************************************************/
 
 #include <stdio.h>
@@ -19,9 +31,11 @@
 
 int main(int argc, char **argv)
 {
-    int i;
+    int dev;
 
     printf("PDOS should reside on cylinder 1, head 0\n");
     printf("and one day we'll even load it. :-)\n");
+    dev = initsys();
+    printf("IPL device is %x\n", dev);
     return (0);
 }

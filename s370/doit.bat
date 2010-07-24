d90253f08335ada7f2993201b023be05af8a7dfd
kerravon86 <kerravon86>
2010-07-24 06:02:05 +0000

    use syntax for separate bootstrap loader

--- s370/doit.bat
+++ s370/doit.bat
@@ -18,7 +18,7 @@ call runmvs pdosload.jcl output.txt none pdosload.txt binary ascii
 hdtofil pdosload.bin <pdosload.txt
 echo 000000  C1C2C2C1 >dummy.txt
 hdtofil pdos.bin <dummy.txt
-echo PDOS00 3390-1 * pdosload.bin >ctl.txt
+echo PDOS00 3390-1 * separate >ctl.txt
 echo SYS1.PLOAD SEQ pdosload.bin TRK 10 1 0 PS U 0 18452 >>ctl.txt
 echo SYS1.PDOS SEQ pdos.bin CYL 1 1 0 PS U 0 18452 >>ctl.txt
 del pdos00.199

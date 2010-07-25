33e73c918dcfc3467bd7d9821d908f9b50296770
kerravon86 <kerravon86>
2010-07-25 02:42:45 +0000

    main operating system

---
+++ s370/pdos.c
@@ -0,0 +1,76 @@
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  pdos.c - Public Domain Operating System                          */
+/*                                                                   */
+/*  When this (main) program is entered, interrupts are disabled,    */
+/*  and the program should not assume anything about the status of   */
+/*  low memory - ie this program needs to set the interrupt vectors, */
+/*  initialize timer, and enable interrupts.                         */
+/*                                                                   */
+/*  To cope with multitasking, and multiple CPUs, from the outset,   */
+/*  the following strategy is being used:                            */
+/*                                                                   */
+/*  Given that most time is spent in an application, rather than     */
+/*  doing OS work, we can envision that 4 CPUs are busy doing some   */
+/*  calculations, e.g. LA R2,7(R2). They each have their own version */
+/*  of the registers, and they have each been loaded in advance      */
+/*  with virtual memory settings that only give access to real       */
+/*  memory that doesn't clash with any other CPU.                    */
+/*                                                                   */
+/*  Each of those CPUs can generate an interrupt at any time, by     */
+/*  executing an illegal instruction, or having an expired timer,    */
+/*  or requesting a service.                                         */
+/*                                                                   */
+/*  Each time there is an interrupt, interrupts will be switched     */
+/*  off. The CPU receiving the interrupt needs to save its           */
+/*  registers, and return to the point at which it last dispatched   */
+/*  some interruptable work.                                         */
+/*                                                                   */
+/*  So a function like dispatch_until_interrupt() should be called,  */
+/*  and it is expected to return with an interrupt identifier.       */
+/*                                                                   */
+/*  Since the interrupt may be a program check etc, this task        */
+/*  should remain suspended until the interrupt has been             */
+/*  analyzed. So what we're after is:                                */
+/*                                                                   */
+/*  while (!time_to_terminate)                                       */
+/*  {                                                                */
+/*      dispatch_until_interrupt();                                  */
+/*      if (interrupt == ILLEGAL_INSTRUCTION) time_to_terminate = 1; */
+/*      else if (interrupt == TIMER)                                 */
+/*      {                                                            */
+/*          checkElapsedTimeAgainstAllowedTime();                    */
+/*          if (over_time_limit) time_to_terminate = 1;              */
+/*      }                                                            */
+/*  }                                                                */
+/*                                                                   */
+/*  In order to effect this simplicity, what that                    */
+/*  dispatch_until_interrupt function translates into is:            */
+/*                                                                   */
+/*  LPSW of user's application code, with interrupts enabled.        */
+/*  All interrupts point to the address after that LPSW.             */
+/*                                                                   */
+/*  Registers are provided and loaded prior to execution of the      */
+/*  LPSW. The LPSW will refer to an address in low memory so that    */
+/*  the (now invalid) register 0 can be used.                        */
+/*                                                                   */
+/*********************************************************************/
+
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+    printf("Welcome to PDOS!!!\n");
+    return (0);
+}

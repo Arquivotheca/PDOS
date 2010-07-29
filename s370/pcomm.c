c50e734e8a3e72c55f0c250d43d5dd1c4ad9ac10
kerravon86 <kerravon86>
2010-07-29 10:43:51 +0000

    command processor for PDOS

---
+++ s370/pcomm.c
@@ -0,0 +1,22 @@
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  pcomm - command processor for pdos                               */
+/*                                                                   */
+/*********************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+    printf("welcome to pcomm\n");
+    return (0);
+}

30c85c60d7c78f026bb907a9655da4b20003ce9d
kerravon86 <kerravon86>
2010-07-30 23:27:43 +0000

    print DCB info

--- s370/pcomm.c
+++ s370/pcomm.c
@@ -18,6 +18,11 @@
 int main(int argc, char **argv)
 {
     printf("welcome to pcomm\n");
+#if 0
+    /* need to get the DCB open exit working first */
+    printf("lrecl %d, recfm %x, blksize %d\n", stdout->lrecl,
+           stdout->recfm, stdout->blksize);
+#endif
     printf("what a relief!\n");
     return (5);
 }

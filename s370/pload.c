49338c2f581effaf6491206a7214afa9dafdd597
kerravon86 <kerravon86>
2010-09-03 11:23:44 +0000

    use defines

--- s370/pload.c
+++ s370/pload.c
@@ -33,15 +33,37 @@ static char buf[40000];
 
 #define CHUNKSZ 18452
 
-#define PDOS_CODE 0x200000
-#define PDOS_HEAP 0x300000
+/* pload is a standalone loader, and thus is normally loaded
+   starting at location 0. */
+#define PLOAD_START 0x0
+
+/* when pload is directly IPLed, and thus starts from the
+   address in location 0, it knows to create its own stack,
+   which it does at 0.5 MB in (thus creating a restriction
+   of only being 0.5 MB in size unless this is changed */
+#define PLOAD_STACK (PLOAD_START + 0x080000)
+
+/* the heap - for the equivalent of getmains - is located
+   another 0.5 MB in, ie at the 1 MB location */
+#define PLOAD_HEAP (PLOAD_STACK + 0x080000)
+
+/* PDOS is loaded another 1 MB above the PLOAD heap - ie the 2 MB location */
+#define PDOS_CODE (PLOAD_HEAP + 0x100000)
+
+/* standard offset for a standalone PDPCLIB program that doesn't need
+   to load itself, since it wasn't directly IPLed */
+#define PDOS_ENTRY (PDOS_CODE + 0x800)
+
+/* The heap starts 1 MB after the code (ie the 3 MB location).
+   So PDOS can't be more than 1 MB in size unless this is changed */
+#define PDOS_HEAP (PDOS_CODE + 0x100000)
 
 int main(int argc, char **argv)
 {
     int dev;
     char *load;
     char *start = (char *)PDOS_CODE;
-    void (*entry)(void *) = (void (*)(void *))0x200800;
+    void (*entry)(void *) = (void (*)(void *))PDOS_ENTRY;
     int i;
     int j;
     /* programs using the standalone version of PDPCLIB need

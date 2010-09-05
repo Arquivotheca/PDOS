5808e8215265480cf94584d599a6cea04d8840cd
kerravon86 <kerravon86>
2010-09-05 22:36:31 +0000

    provide input file

--- s370/doit.bat
+++ s370/doit.bat
@@ -21,10 +21,12 @@ unzip -o pload
 hdtofil pload.bin <pload.txt
 hdtofil pdos.bin <pdos.txt
 hdtofil pcomm.bin <pcomm.txt
+echo 1234 >pcomm.in
 echo PDOS00 3390-1 * separate >ctl.txt
 echo SYS1.PLOAD SEQ pload.bin TRK 10 1 0 PS U 0 18452 >>ctl.txt
 echo SYS1.PDOS SEQ pdos.bin CYL 1 1 0 PS U 0 18452 >>ctl.txt
-echo SYS1.PCOMM SEQ pcomm.bin CYL 4 1 0 PS U 0 18452 >>ctl.txt
+echo SYS1.PCOMM SEQ pcomm.bin CYL 1 1 0 PS U 0 18452 >>ctl.txt
+echo SYS1.PCOMM.IN SEQ pcomm.in CYL 1 1 0 PS U 0 18452 >>ctl.txt
 del pdos00.199
 dasdload -bz2 ctl.txt pdos00.199
 copy pdos00.199 \mvs380\dasd

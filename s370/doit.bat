f5103bac305e03e5d6d4f0130f04127cfd954962
kerravon86 <kerravon86>
2010-09-12 01:42:25 +0000

    return to normal PCOMM

--- s370/doit.bat
+++ s370/doit.bat
@@ -25,7 +25,7 @@ copy pcommin.txt pcomm.in
 echo PDOS00 3390-1 * separate >ctl.txt
 echo SYS1.PLOAD SEQ pload.bin TRK 10 1 0 PS U 0 18452 >>ctl.txt
 echo SYS1.PDOS SEQ pdos.bin CYL 1 1 0 PS U 0 18452 >>ctl.txt
-echo SYS1.PCOMM SEQ pcomm.bin CYL 4 1 0 PS U 0 18452 >>ctl.txt
+echo SYS1.PCOMM SEQ pcomm.bin CYL 1 1 0 PS U 0 18452 >>ctl.txt
 echo SYS1.PCOMM.IN SEQ pcomm.in CYL 1 1 0 PS U 0 18452 >>ctl.txt
 del pdos00.199
 dasdload -bz2 ctl.txt pdos00.199

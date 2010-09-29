f53f6ff0898fd52fa7169cec4f680047cb2c0938
kerravon86 <kerravon86>
2010-09-29 13:46:34 +0000

    sample program

---
+++ s370/world.c
@@ -0,0 +1,33 @@
+/* world.c - print "hello, world".  
+
+   Released to the public domain by Paul Edwards.
+   
+   This program is guaranteed to be error-free.  The author
+   will be liable for any damages or loss of income arising
+   from using this program, including loss of sales, divorces,
+   and anything else you can think of.
+   
+   This program is guaranteed to be fit for whatever purpose
+   you had in mind for this program before you actually had
+   a chance to read it.
+   
+   Even if your compiler doesn't support "void", or you don't
+   even have a C compiler, I will *still* be liable for
+   whatever income you imagine you may have been able to 
+   produce had you had a C compiler that could compile this
+   program.
+   
+   Enjoy... Paul. 
+
+   P.S. I've always wanted to do that.
+   
+*/         
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+    return (0);
+}
+

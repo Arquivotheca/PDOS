5818bd295163e05547eac26047bfd6d1a48d563d
kerravon86 <kerravon86>
2010-09-30 22:34:23 +0000

    world that prints arguments

--- s370/world.c
+++ s370/world.c
@@ -1,33 +1,35 @@
-/* world.c - print "hello, world".  
-
-   Released to the public domain by Paul Edwards.
-   
-   This program is guaranteed to be error-free.  The author
-   will be liable for any damages or loss of income arising
-   from using this program, including loss of sales, divorces,
-   and anything else you can think of.
-   
-   This program is guaranteed to be fit for whatever purpose
-   you had in mind for this program before you actually had
-   a chance to read it.
-   
-   Even if your compiler doesn't support "void", or you don't
-   even have a C compiler, I will *still* be liable for
-   whatever income you imagine you may have been able to 
-   produce had you had a C compiler that could compile this
-   program.
-   
-   Enjoy... Paul. 
-
-   P.S. I've always wanted to do that.
-   
-*/         
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  world - print arguments                                          */
+/*                                                                   */
+/*********************************************************************/
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-    printf("hello, world\n");
-    return (0);
-}
+    int i;
 
+    printf("welcome to my world\n");
+    printf("argc = %d\n" , argc);
+    for (i = 0; i < argc; i++)
+    {
+        printf("arg %d is <%s>\n", i, argv[i]);
+    }
+    printf("looping for a while\n"); /* about 10 seconds */
+    for (i = 0; i < 20000000; i++)
+    {
+        ;
+    }
+    printf("finished looping\n");
+    return (5);
+}

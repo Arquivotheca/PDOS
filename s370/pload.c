c79c499e10fb624e9e4d28736246675b39477f76
kerravon86 <kerravon86>
2010-10-11 13:33:28 +0000

    look pdos location up via VTOC

--- s370/pload.c
+++ s370/pload.c
@@ -60,14 +60,18 @@ static char buf[40000];
    So PDOS can't be more than 1 MB in size unless this is changed */
 #define PDOS_HEAP (PDOS_CODE + 0x100000)
 
+static int ipldev;
+
+static int findFile(char *dsn, int *c, int *h, int *r);
+
 int main(int argc, char **argv)
 {
-    int dev;
     char *load;
     char *start = (char *)PDOS_CODE;
     void (*entry)(void *) = (void (*)(void *))PDOS_ENTRY;
-    int i;
-    int j;
+    int cyl;
+    int head;
+    int rec;
     /* programs using the standalone version of PDPCLIB need
        to be informed where they can allocate a heap and
        potentially other parameters (that's why there is a
@@ -81,28 +85,129 @@ int main(int argc, char **argv)
 #if 0
     printf("PDOS should reside on cylinder 1, head 0 of IPL device\n");
 #endif
-    dev = initsys();
+    ipldev = initsys();
 #if 0
-    printf("IPL device is %x\n", dev);
+    printf("IPL device is %x\n", ipldev);
 #endif
-    load = start;
-    for (i = 0; i < 10; i++)
+    if (findFile("PDOS.SYS", &cyl, &head, &rec) != 0)
     {
-        for (j = 1; j < 4; j++)
+        printf("PDOS.SYS not found\n");
+        return (12);
+    }
+    load = start;
+    for (head = 0; head < 10; head++)
+    {
+        for (rec = 1; rec < 4; rec++)
         {
 #if 0
-            printf("loading to %p from 1, %d, %d\n", load, i, j);
+            printf("loading to %p from %d, %d, %d\n", load, cyl, head, rec);
 #endif
-            rdblock(dev, 1, i, j, load, CHUNKSZ);
+            rdblock(ipldev, cyl, head, rec, load, CHUNKSZ);
             load += CHUNKSZ;
         }
     }
     entry(&pblock);
 #if 0
     printf("about to read first block of PDOS\n");
-    rdblock(dev, 1, 0, 1, buf, sizeof buf);
+    rdblock(ipldev, cyl, head, rec, buf, sizeof buf);
     printf("got %x %x %x %x\n", buf[0], buf[1], buf[2], buf[3]);
     printf("and %x %x %x %x\n", buf[4], buf[5], buf[6], buf[7]);
 #endif
+    return (0);
+}
+
+
+/* find a file on disk */
+
+#define MAXBLKSZ 32767
+
+static int findFile(char *dsn, int *c, int *h, int *r)
+{
+    char *raw;
+    char *initial;
+    char *load;
+    /* Standard C programs can start at a predictable offset */
+    int (*entry)(void *);
+    int cyl;
+    int head;
+    int rec;
+    int i;
+    int j;
+    static int savearea[20]; /* needs to be in user space */
+    static char *pptrs[1];
+    char tbuf[MAXBLKSZ];
+    char srchdsn[FILENAME_MAX+10]; /* give an extra space */
+    int cnt = -1;
+    int lastcnt = 0;
+    int ret = 0;
+    struct {
+        char ds1dsnam[44];
+        char ds1fmtid;
+        char unused1[60];
+        char unused2;
+        char unused3;
+        char startcchh[4];
+        char endcchh[4];
+    } dscb1;
+    int len;
+
+    if (memchr(dsn, '\0', FILENAME_MAX) == NULL)
+    {
+        len = FILENAME_MAX;
+    }
+    else
+    {
+        len = strlen(dsn);
+    }
+    memcpy(srchdsn, dsn, len);
+    strcpy(srchdsn + len, " ");
+    len++; /* force a search for the blank */
+    
+    /* read VOL1 record which starts on cylinder 0, head 0, record 3 */
+    cnt = rdblock(ipldev, 0, 0, 3, tbuf, MAXBLKSZ);
+    if (cnt >= 20)
+    {
+        cyl = head = rec = 0;
+        /* +++ probably time to create some macros for this */
+        memcpy((char *)&cyl + sizeof(int) - 2, tbuf + 15, 2);
+        memcpy((char *)&head + sizeof(int) - 2, tbuf + 17, 2);
+        memcpy((char *)&rec + sizeof(int) - 1, tbuf + 18, 1);
+        
+        while ((cnt =
+               rdblock(ipldev, cyl, head, rec, &dscb1, sizeof dscb1))
+               > 0)
+        {
+            if (cnt >= sizeof dscb1)
+            {
+                if (dscb1.ds1fmtid == '1')
+                {
+                    dscb1.ds1fmtid = ' '; /* for easy comparison */
+                    if (memcmp(dscb1.ds1dsnam,
+                               srchdsn,
+                               len) == 0)
+                    {
+                        cyl = head = 0;
+                        rec = 1;
+                        /* +++ more macros needed here */
+                        memcpy((char *)&cyl + sizeof(int) - 2, 
+                               dscb1.startcchh, 2);
+                        memcpy((char *)&head + sizeof(int) - 2,
+                               dscb1.startcchh + 2, 2);
+                        break;
+                    }
+                }
+            }
+            rec++;
+        }        
+    }
+    
+    if (cnt <= 0)
+    {
+        /* not found */
+        return (-1);
+    }
+    *c = cyl;
+    *h = head;
+    *r = rec;
     return (0);
 }

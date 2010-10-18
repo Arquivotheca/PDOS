a9a1379da42b7b6446021317140790e3a1e1f36d
kerravon86 <kerravon86>
2010-10-18 09:41:30 +0000

    switch on optimization

--- s370/doit.bat
+++ s370/doit.bat
@@ -1,20 +1,20 @@
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/start.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/stdio.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/stdlib.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/ctype.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/string.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/time.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/errno.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/assert.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/locale.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/math.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/setjmp.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/signal.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib ../pdpclib/__memmgr.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib pload.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib pdos.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib pcomm.c
-gccmvs -DUSE_MEMMGR -DS380 -S -I . -I ../pdpclib world.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/start.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/stdio.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/stdlib.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/ctype.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/string.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/time.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/errno.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/assert.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/locale.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/math.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/setjmp.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/signal.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/__memmgr.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib pload.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib pdos.c
+gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib pcomm.c
+gccmvs -DUSE_MEMMGR -O0 -DS380 -S -I . -I ../pdpclib world.c
 
 rem when globally changing, change the DS3x0 to whatever
 rem and then change the single conf file to the right one
@@ -30,6 +30,7 @@ copy pcomm.txt pcomm.bin
 copy pcommin.txt pcomm.in
 copy world.txt world.bin
 copy sample.txt sample.c
+copy gcc.txt gcc.bin
 echo PDOS00 3390-1 * separate >ctl.txt
 echo PLOAD.SYS SEQ pload.bin TRK 15 1 0 PS U 0 18452 >>ctl.txt
 echo SYSVTOC VTOC TRK 10 >>ctl.txt
@@ -39,6 +40,7 @@ echo COMMAND.EXE SEQ pcomm.bin TRK 15 1 0 PS U 0 18452 >>ctl.txt
 echo AUTOEXEC.BAT SEQ pcomm.in TRK 3 1 0 PS U 0 18452 >>ctl.txt
 echo WORLD.EXE SEQ world.bin TRK 15 1 0 PS U 0 18452 >>ctl.txt
 echo SAMPLE.C SEQ sample.c TRK 3 1 0 PS U 0 18452 >>ctl.txt
+echo GCC.EXE SEQ gcc.bin CYL 4 1 0 PS U 0 18452 >>ctl.txt
 del pdos00.199
 dasdload -bz2 ctl.txt pdos00.199
 copy pdos00.199 \mvs380\dasd

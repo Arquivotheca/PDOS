7d14df74904dac9868b729cf53cc94dd16082c80
kerravon86 <kerravon86>
2010-10-31 02:22:57 +0000

    do PDOS-specific compiles

--- s370/doit.bat
+++ s370/doit.bat
@@ -1,21 +1,21 @@
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/start.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/stdio.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/stdlib.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/ctype.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/string.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/time.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/errno.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/assert.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/locale.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/math.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/setjmp.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/signal.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/__memmgr.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib pload.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib pdos.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib pdosutil.c
-gccmvs -DUSE_MEMMGR -Os -DS380 -S -I . -I ../pdpclib pcomm.c
-gccmvs -DUSE_MEMMGR -O0 -DS380 -S -I . -I ../pdpclib world.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/start.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/stdio.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/stdlib.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/ctype.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/string.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/time.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/errno.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/assert.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/locale.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/math.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/setjmp.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/signal.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib ../pdpclib/__memmgr.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib pload.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib pdos.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib pdosutil.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS380 -S -I . -I ../pdpclib pcomm.c
+gccmvs -DUSE_MEMMGR -D__PDOS__ -O0 -DS380 -S -I . -I ../pdpclib world.c
 
 rem when globally changing, change the DS3x0 to whatever
 rem and then change the single conf file to the right one

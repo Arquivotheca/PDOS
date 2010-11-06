154356246768e964aaef9b13c016538ecfb5701d
kerravon86 <kerravon86>
2010-11-06 04:33:35 +0000

    continued revamping

--- s370/doit.bat
+++ s370/doit.bat
@@ -17,14 +17,23 @@ gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS390 -S -I . -I ../pdpclib pdosutil.c
 gccmvs -DUSE_MEMMGR -D__PDOS__ -Os -DS390 -S -I . -I ../pdpclib pcomm.c
 gccmvs -DUSE_MEMMGR -D__PDOS__ -O0 -DS390 -S -I . -I ../pdpclib world.c
 
+sleep 2
+
 rem when globally changing, change the DS3x0 to whatever
-rem and then change the single conf file to the right one
+rem and then change the single conf file in the next line to the right one
 
 copy pdos390.cnf pdos.cnf
 
 
+rem we did compiles already, but now need to do assembles and
+rem links, plus iebcopy unloads, so we use MVS for that
+
 m4 -I . -I ../pdpclib pdos.m4 >pdos.jcl
 call runmvs pdos.jcl output.txt none pdos.zip
+
+
+rem extract everything and put into expected names
+
 unzip -o pdos
 copy pload.txt pload.sys
 copy pdos.txt pdos.sys
@@ -36,9 +45,15 @@ copy sample.txt sample.c
 copy wtoworld.txt wtoworld.exe
 copy diff.txt diff.exe
 
+
+rem create dummy batch file
+
 echo echo nothing to run >hercauto.bat
 del hercauto.zip
 zip -0X hercauto hercauto.bat
+
+
+rem create package suitable for "shipping"
 
 del pdospkg.zip
 zip -9X pdospkg pload.sys pdos.sys config.sys 
@@ -46,11 +61,20 @@ zip -9X pdospkg pcomm.exe autoexec.bat world.exe sample.c
 zip -9X pdospkg wtoworld.exe diff.exe hercauto.zip
 zip -9X pdospkg ctl.txt
 
+
+rem get GCC/PDPCLIB utilities
+
 unzip -o gccpdos
+
+
+rem build DASD. Put a copy into MVS/380 area for no particular reason
 
 del pdos00.cckd
 dasdload -bz2 ctl.txt pdos00.cckd
-copy pdos00.cckd \mvs380\dasd\pdos00.199
+copy pdos00.cckd %MVS380%\dasd\pdos00.199
+
+
+rem Try out the new version of PDOS
 
 set HERCULES_RC=auto_ipl.rc
 hercules -f pdos.cnf >hercules.log

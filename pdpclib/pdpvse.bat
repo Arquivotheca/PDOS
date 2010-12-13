baa730dd400791974139a2b160db4353638cf59e
kerravon86 <kerravon86>
2010-12-13 12:33:23 +0000

    VSE now asserted

--- pdpclib/pdpvse.bat
+++ pdpclib/pdpvse.bat
@@ -1,18 +1,18 @@
 del pdpvse.zip
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . start.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . stdio.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . stdlib.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . ctype.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . string.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . time.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . errno.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . assert.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . locale.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . math.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . setjmp.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . signal.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . __memmgr.c
-gccmvs -DUSE_MEMMGR -ansi -pedantic-errors -Os -D__VSE__ -S -I . pdptest.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . start.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . stdio.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . stdlib.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . ctype.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . string.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . time.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . errno.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . assert.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . locale.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . math.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . setjmp.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . signal.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . __memmgr.c
+gccvse -DUSE_MEMMGR -ansi -pedantic-errors -Os -S -I . pdptest.c
 rem zip -0X pdpvse *.s *.exec *.asm *.mac
 
 m4 -I . pdpvse.m4 >pdpvse.jcl

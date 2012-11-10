3a275bd4246a818ce2bc94baada438033f57226a
kerravon86 <kerravon86>
2012-11-10 21:55:21 +0000

    explain what needs to be in the first 3 sectors

--- src/comp1.bat
+++ src/comp1.bat
@@ -9,4 +9,5 @@ wasmr -zq -zcm -Dmemodel=tiny lldos.asm
 tcc -O -c -mt -DNEED_DUMP -I..\pdpclib pload.c fat.c bos.c pdosload.c ..\pdpclib\string.c ..\pdpclib\dossupc.c
 rem the order of the following stuff is critical so that the first 3*512
 rem bytes have sufficient stuff to do the rest of the load of itself!!!
+rem it is ploadst, pload, int13x and near that need to be in the first 3*512
 tlink -t -x ploadst+pload+int13x+near+support+bos+dossupc+fat+string+pdosload+lldos,pload.com,,,

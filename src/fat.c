564cc1152bd7ec16d508e302078b7faad956f057
kerravon86 <kerravon86>
2013-03-10 12:05:30 +0000

    bug fix

--- src/fat.c
+++ src/fat.c
@@ -632,7 +632,9 @@ static void fatDirSectorUpdate(FAT *fat,
             if (*p == '\0')
             {
                 fatfile->cluster = 50; /* +++ */
-                fatfile->sectorStart = 50 * (2048/512); /* +++ */
+                fatfile->sectorStart = 50 * (2048/512)
+                    + fat->rootstart + fat->rootsize
+                    - 2 * (2048/512); /* +++ */                
                 fatfile->sectorUpto = 0; /* +++ */
                 memset(p, '\0', 32);
                 memcpy(p, search, 11);

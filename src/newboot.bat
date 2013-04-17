f013b635b4fb5b3e8a85d217bd4650f77f2197a5
kerravon86 <kerravon86>
2013-04-17 11:47:05 +0000

    create a new boot sector

---
+++ src/newboot.bat
@@ -0,0 +1,6 @@
+sectread 81 63 boot.com
+xychop pbootsec.com boot.001 0 10
+xychop pbootsec.com boot.003 62 511
+xychop boot.com boot.002 11 61
+copy /b boot.001+boot.002+boot.003 newboot.com
+rem sectwrit 81 63 newboot.com

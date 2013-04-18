e0589dae62247401d7d788044033d08cc87c1422
kerravon86 <kerravon86>
2013-04-18 09:05:34 +0000

    bug fixes

--- src/newboot.bat
+++ src/newboot.bat
@@ -1,6 +1,6 @@
 sectread 81 63 boot.com
 xychop pbootsec.com boot.001 0 10
-xychop pbootsec.com boot.003 62 511
-xychop boot.com boot.002 11 61
+xychop pbootsec.com boot.003 73 511
+xychop boot.com boot.002 11 72
 copy /b boot.001+boot.002+boot.003 boot2.com
 sectwrit 81 63 boot2.com

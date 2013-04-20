3b0ef12bd3a12b178046993bdd348a9e23a00d49
kerravon86 <kerravon86>
2013-04-20 08:18:18 +0000

    add doco

--- src/pbootsec.asm
+++ src/pbootsec.asm
@@ -36,6 +36,57 @@ jmp bypass
 nop
 
 sysname db 'PDOS x.x'
+
+; HD:
+;
+; 000000  EB479050 444F5320 782E78
+;                                 00 02        bytes per sector
+;                                      04      sectors per cluster
+;                                        0100  # reserved
+;                                              .G.PDOS x.x.....
+; 000010  02                                   # fats
+;           0002                               # directory entries (512)
+;               01 EC                          total logical sectors
+;                    F8                        media byte
+;                      3B00                    sectors per FAT
+; 
+; 3b=59*2 + 32 (directory sectors) + 1 (reserved) = 151
+; 
+;                           3F001000 3F000000  ......;.?...?...
+; 000020  00000000 800029EC 1C591E4E 4F204E41  ......)..Y.NO NA
+; 000030  4D452020 20204641 54313620 202031C0  ME    FAT16   1.
+; 000040  FA8ED0BC 007CFB0E 1FB80000 8ED0BC00  .....|..........
+; 000050  7C0E1FE8 5D00B870 0050B800 0050CB06  |...]..p.P...P..
+; 000060  53505152 1EB80000 8EC0BB78 00268B47  SPQR.......x.&.G
+; 000070  028ED826 8B078BD0 B800008E C0BB3E7C  ...&..........>|
+; 000080  B9000083 F90B7D0E 538BDA8A 075B2688  ......}.S....[&.
+; 000090  07424341 EBEDB800 008EC0BB 780026C7  .BCA........x.&.
+; 
+; floppy:
+; 
+; 000000  EB3C904C 494E5558 342E31
+;                                 00 02        bytes per sector
+;                                      01      sectors per cluster
+;                                        0100  # reserved
+;                                              .<.LINUX4.1.....
+; 000010  02                                   # fats
+;           E000                               # directory entries (224)
+;               40 0B                          total logical sectors
+;                    F0                        media byte
+;                      0900                    sectors per FAT
+; 
+; 9*2 + 224*32/512 + 1 = 33
+;                           12000200 00000000  ...@............
+; 000020  00000000 000029E3 7FFA4420 20202020  ......)...D
+; 000030  20202020 20204641 54313220 2020FAFC        FAT12   ..
+; 000040  31C08ED8 BD007CB8 E01F8EC0 89EE89EF  1.....|.........
+; 000050  B90001F3 A5EA5E7C E01F0000 60008ED8  ......^|....`...
+; 000060  8ED08D66 A0FB807E 24FF7503 885624C7  ...f...~$.u..V$.
+; 000070  46C01000 C746C201 00E8E900 46726565  F....F......Free
+; 000080  444F5300 8B761C8B 7E1E0376 0E83D700  DOS..v..~..v....
+; 000090  8976D289 7ED48A46 1098F766 1601C611  .v..~..F...f....
+
+
 bpb db 51 dup(?)
 
 ; new disk parameter table

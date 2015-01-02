29afa58b55d8493bb1e102cc4b63a1ce984df2cf
kerravon86 <kerravon86>
2015-01-02 08:02:46 +0000

    document ZAPBLK

--- s370/pcomm.c
+++ s370/pcomm.c
@@ -289,6 +289,7 @@ static void dohelp(void)
     printf("HELP - display this help\n");
     printf("TYPE - display contents of a file\n");
     printf("DUMPBLK - dump a block on disk\n");
+    printf("ZAPBLK - zap a block on disk\n");
     printf("DIR - display directory\n");
     printf("SHOWRC - display return code from programs\n");
     printf("EXIT - exit operating system\n");

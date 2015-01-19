1ca8fae1542274c72a90e9143ef53643dccf3fb4
kerravon86 <kerravon86>
2015-01-19 04:42:24 +0000

    don't use MEMMGR by default

--- pdpclib/pdpmvs.bat
+++ pdpclib/pdpmvs.bat
@@ -1,17 +1,17 @@
-gccmvs -DUSE_MEMMGR -S -I . start.c
-gccmvs -DUSE_MEMMGR -S -I . stdio.c
-gccmvs -DUSE_MEMMGR -S -I . stdlib.c
-gccmvs -DUSE_MEMMGR -S -I . ctype.c
-gccmvs -DUSE_MEMMGR -S -I . string.c
-gccmvs -DUSE_MEMMGR -S -I . time.c
-gccmvs -DUSE_MEMMGR -S -I . errno.c
-gccmvs -DUSE_MEMMGR -S -I . assert.c
-gccmvs -DUSE_MEMMGR -S -I . locale.c
-gccmvs -DUSE_MEMMGR -S -I . math.c
-gccmvs -DUSE_MEMMGR -S -I . setjmp.c
-gccmvs -DUSE_MEMMGR -S -I . signal.c
-gccmvs -DUSE_MEMMGR -S -I . __memmgr.c
-gccmvs -DUSE_MEMMGR -S -I . pdptest.c
+gccmvs -DXXX_MEMMGR -S -I . start.c
+gccmvs -DXXX_MEMMGR -S -I . stdio.c
+gccmvs -DXXX_MEMMGR -S -I . stdlib.c
+gccmvs -DXXX_MEMMGR -S -I . ctype.c
+gccmvs -DXXX_MEMMGR -S -I . string.c
+gccmvs -DXXX_MEMMGR -S -I . time.c
+gccmvs -DXXX_MEMMGR -S -I . errno.c
+gccmvs -DXXX_MEMMGR -S -I . assert.c
+gccmvs -DXXX_MEMMGR -S -I . locale.c
+gccmvs -DXXX_MEMMGR -S -I . math.c
+gccmvs -DXXX_MEMMGR -S -I . setjmp.c
+gccmvs -DXXX_MEMMGR -S -I . signal.c
+gccmvs -DXXX_MEMMGR -S -I . __memmgr.c
+gccmvs -DXXX_MEMMGR -S -I . pdptest.c
 m4 -I . pdpmvs.m4 >pdpmvs.jcl
 rem call sub pdpmvs.jcl
 call runmvs pdpmvs.jcl output.txt

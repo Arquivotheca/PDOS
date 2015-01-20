f6cb6e8fd05d69ee085591b58c59d69a88c28943
kerravon86 <kerravon86>
2015-01-20 08:41:06 +0000

    compile with optimization on

--- pdpclib/pdpmvs.bat
+++ pdpclib/pdpmvs.bat
@@ -1,17 +1,17 @@
-gccmvs -DXXX_MEMMGR -S -I . start.c
-gccmvs -DXXX_MEMMGR -S -I . stdio.c
-gccmvs -DXXX_MEMMGR -S -I . stdlib.c
-gccmvs -DXXX_MEMMGR -S -I . ctype.c
-gccmvs -DXXX_MEMMGR -S -I . string.c
-gccmvs -DXXX_MEMMGR -S -I . time.c
-gccmvs -DXXX_MEMMGR -S -I . errno.c
-gccmvs -DXXX_MEMMGR -S -I . assert.c
-gccmvs -DXXX_MEMMGR -S -I . locale.c
-gccmvs -DXXX_MEMMGR -S -I . math.c
-gccmvs -DXXX_MEMMGR -S -I . setjmp.c
-gccmvs -DXXX_MEMMGR -S -I . signal.c
-gccmvs -DXXX_MEMMGR -S -I . __memmgr.c
-gccmvs -DXXX_MEMMGR -S -I . pdptest.c
+gccmvs -Os -DXXX_MEMMGR -S -I . start.c
+gccmvs -Os -DXXX_MEMMGR -S -I . stdio.c
+gccmvs -Os -DXXX_MEMMGR -S -I . stdlib.c
+gccmvs -Os -DXXX_MEMMGR -S -I . ctype.c
+gccmvs -Os -DXXX_MEMMGR -S -I . string.c
+gccmvs -Os -DXXX_MEMMGR -S -I . time.c
+gccmvs -Os -DXXX_MEMMGR -S -I . errno.c
+gccmvs -Os -DXXX_MEMMGR -S -I . assert.c
+gccmvs -Os -DXXX_MEMMGR -S -I . locale.c
+gccmvs -Os -DXXX_MEMMGR -S -I . math.c
+gccmvs -Os -DXXX_MEMMGR -S -I . setjmp.c
+gccmvs -Os -DXXX_MEMMGR -S -I . signal.c
+gccmvs -Os -DXXX_MEMMGR -S -I . __memmgr.c
+gccmvs -Os -DXXX_MEMMGR -S -I . pdptest.c
 m4 -I . pdpmvs.m4 >pdpmvs.jcl
 rem call sub pdpmvs.jcl
 call runmvs pdpmvs.jcl output.txt

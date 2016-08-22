1a4dcccdf1cd14c4ec62f71f494bb2ffc49ff7d7
kerravon86 <kerravon86>
2016-08-22 08:19:28 +0000

    switch off memmgr

--- pdpclib/pdpcms.bat
+++ pdpclib/pdpcms.bat
@@ -1,18 +1,18 @@
 del pdpcms.zip
-gcccms -Os -DUSE_MEMMGR -S -I . start.c
-gcccms -Os -DUSE_MEMMGR -S -I . stdio.c
-gcccms -Os -DUSE_MEMMGR -S -I . stdlib.c
-gcccms -Os -DUSE_MEMMGR -S -I . ctype.c
-gcccms -Os -DUSE_MEMMGR -S -I . string.c
-gcccms -Os -DUSE_MEMMGR -S -I . time.c
-gcccms -Os -DUSE_MEMMGR -S -I . errno.c
-gcccms -Os -DUSE_MEMMGR -S -I . assert.c
-gcccms -Os -DUSE_MEMMGR -S -I . locale.c
-gcccms -Os -DUSE_MEMMGR -S -I . math.c
-gcccms -Os -DUSE_MEMMGR -S -I . setjmp.c
-gcccms -Os -DUSE_MEMMGR -S -I . signal.c
-gcccms -Os -DUSE_MEMMGR -S -I . __memmgr.c
-gcccms -Os -DUSE_MEMMGR -S -I . pdptest.c
+gcccms -Os -DXXX_MEMMGR -S -I . start.c
+gcccms -Os -DXXX_MEMMGR -S -I . stdio.c
+gcccms -Os -DXXX_MEMMGR -S -I . stdlib.c
+gcccms -Os -DXXX_MEMMGR -S -I . ctype.c
+gcccms -Os -DXXX_MEMMGR -S -I . string.c
+gcccms -Os -DXXX_MEMMGR -S -I . time.c
+gcccms -Os -DXXX_MEMMGR -S -I . errno.c
+gcccms -Os -DXXX_MEMMGR -S -I . assert.c
+gcccms -Os -DXXX_MEMMGR -S -I . locale.c
+gcccms -Os -DXXX_MEMMGR -S -I . math.c
+gcccms -Os -DXXX_MEMMGR -S -I . setjmp.c
+gcccms -Os -DXXX_MEMMGR -S -I . signal.c
+gcccms -Os -DXXX_MEMMGR -S -I . __memmgr.c
+gcccms -Os -DXXX_MEMMGR -S -I . pdptest.c
 zip -0X pdpcms *.s *.exec *.asm *.mac
 
 rem Useful for VM/380

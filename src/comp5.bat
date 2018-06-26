0d9bbd3f1de96cceb86f9f21fa9175e87330ef03
Paul Edwards <mutazilah@gmail.com>
2018-06-27 01:06:53 +1000

    compile PDOS as relocatable

--- src/comp5.bat
+++ src/comp5.bat
@@ -1,5 +1,5 @@
-gcc -c -I../pdpclib strt32.s pdos.c bos.c fat.c ../pdpclib/string.c ../pdpclib/ctype.c
-gcc -c -I../pdpclib patmat.c support.s memmgr.c protintp.c protints.s pdoss.s
-ld -s -o pdos strt32.o pdos.o bos.o fat.o memmgr.o patmat.o support.o protintp.o protints.o pdoss.o ../pdpclib/pdos.a
+gcc -fno-common -c -I../pdpclib strt32.s pdos.c bos.c fat.c ../pdpclib/string.c ../pdpclib/ctype.c
+gcc -fno-common -c -I../pdpclib patmat.c support.s memmgr.c protintp.c protints.s pdoss.s
+ld -r -s -o pdos strt32.o pdos.o bos.o fat.o memmgr.o patmat.o support.o protintp.o protints.o pdoss.o ../pdpclib/pdos.a
 if exist pdos.exe del pdos.exe
 ren pdos pdos.exe

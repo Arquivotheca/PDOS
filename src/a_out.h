0da6f2853cf2058aae34863116100daaa4e04441
Paul Edwards <mutazilah@gmail.com>
2018-06-27 06:09:27 +1000

    document formula breakdown

--- src/a_out.h
+++ src/a_out.h
@@ -20,6 +20,7 @@ struct exec {
 
 #define N_TXTOFF(e) (0x400)
 #define N_TXTADDR(e) (SEGMENT_SIZE)
+/* this formula doesn't work when the text size is exactly 64k */
 #define N_DATADDR(e) \
     (N_TXTADDR(e) + SEGMENT_SIZE + ((e).a_text & 0xffff0000UL))
 #define N_BSSADDR(e) (N_DATADDR(e) + (e).a_data)

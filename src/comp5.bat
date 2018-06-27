b3f4dccf90b540bccd8d5920b48282b558db71e0
Paul Edwards <mutazilah@gmail.com>
2018-06-28 07:26:50 +1000

    remove unneeded compiles

--- src/comp5.bat
+++ src/comp5.bat
@@ -1,4 +1,4 @@
-gcc -fno-common -c -I../pdpclib strt32.s pdos.c bos.c fat.c ../pdpclib/string.c ../pdpclib/ctype.c
+gcc -fno-common -c -I../pdpclib strt32.s pdos.c bos.c fat.c
 gcc -fno-common -c -I../pdpclib patmat.c support.s memmgr.c protintp.c protints.s pdoss.s
 ld -r -s -o pdos strt32.o pdos.o bos.o fat.o memmgr.o patmat.o support.o protintp.o protints.o pdoss.o ../pdpclib/pdos.a
 if exist pdos.exe del pdos.exe

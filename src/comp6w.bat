5636b5f9296ce84e1cd54cbad7e6e87260dee7cd
Paul Edwards <mutazilah@gmail.com>
2018-06-29 03:22:02 +1000

    cleanup

--- src/comp6w.bat
+++ src/comp6w.bat
@@ -3,8 +3,12 @@ gcc386 -S -Os -fno-common -I../pdpclib -D__32BIT__ pcommrt.c
 gcc386 -S -Os -fno-common -I../pdpclib -D__32BIT__ dostime.c
 
 as386 -o pcomm.o pcomm.s
+del pcomm.s
 as386 -o pcommrt.o pcommrt.s
+del pcommrt.s
 as386 -o dostime.o dostime.s
+del dostime.s
 
-ld386 -r -s -e ___pdosst32 --oformat a.out-i386 -o pcomm.exe ../pdpclib/pdosst32.o pcomm.o pcommrt.o dostime.o ../pdpclib/pdos.a
+ld386 -s -e ___pdosst32 -o pcomm.exe ../pdpclib/pdosst32.o pcomm.o pcommrt.o dostime.o ../pdpclib/pdos.a
+ld386 -r -s -e ___pdosst32 -o pcomm.exe ../pdpclib/pdosst32.o pcomm.o pcommrt.o dostime.o ../pdpclib/pdos.a
 strip386 --strip-unneeded pcomm.exe

a1c3f2f9ee08c11e89d740fd9b6deda744fd3a38
Paul Edwards <mutazilah@gmail.com>
2018-07-01 06:33:42 +1000

    mod from Simon Kissane to use a command table

--- src/pcomm.c
+++ src/pcomm.c
@@ -37,7 +37,13 @@ static struct {
 static size_t len;
 static char drive[2] = "A";
 static char cwd[65];
-static char prompt[50] = ">";
+/* Sometimes I test PCOMM under MS-DOS.
+ * And then I get confused about whether I am in PCOMM or COMMAND.COM.
+ * Making the default prompt for PCOMM different from that of COMMAND.COM
+ * Helps avoid that confusion.
+ * - Simon Kissane
+ */
+static char prompt[50] = "]";
 static int singleCommand = 0;
 static int primary = 0;
 static int term = 0;
@@ -48,20 +54,155 @@ static void putPrompt(void);
 static void doCopy(char *b);
 static void doExec(char *b,char *p);
 static void dotype(char *file);
+static void doEcho(char *msg);
+static void doExit(char *ignored);
 static void dodir(char *pattern);
-static void dover(void);
+static void doReboot(char *ignored);
+static void dover(char *ignored);
 static void dodel(char *fnm);
+static void doPrompt(char *s);
 static void dopath(char *s);
 static void changedir(char *to);
 static void changedisk(int drive);
 static void dohelp(char *cmd);
 static void domkdir(char *dnm);
+static void doMCD(char *dnm);
 static void dormdir(char *dnm);
 static void dorename(char *src);
 static int ins_strcmp(char *one, char *two);
 static int ins_strncmp(char *one, char *two, size_t len);
 static void readBat(char *fnm);
 static int exists(char *fnm);
+
+/* Prototypes for detailed help routines */
+static void help_cd(void);
+static void help_copy(void);
+static void help_del(void);
+static void help_dir(void);
+static void help_echo(void);
+static void help_exit(void);
+static void help_help(void);
+static void help_mcd(void);
+static void help_md(void);
+static void help_path(void);
+static void help_prompt(void);
+static void help_rd(void);
+static void help_reboot(void);
+static void help_ren(void);
+static void help_type(void);
+static void help_ver(void);
+
+/* Function signature for command implementation functions */
+typedef void (*cmdProc)(char *arg);
+
+/* Function signature for command help display functions */
+typedef void (*cmdHelp)(void);
+
+/* Defines an internal command */
+typedef struct cmdBlock
+{
+    char *name;
+    char *description;
+    cmdProc proc;
+    cmdHelp help;
+}
+cmdBlock;
+
+/* The command registry stores definitions of all internal commands.
+ *
+ * Fields:
+ * { name, description, proc, help }
+ *
+ * where:
+ * - name is name of command. Prefix aliases with pipe symbol.
+ * - description is one-line description of command for help system
+ * - proc is procedure to execute for command
+ * - help is detailed help procedure, displays detailed help
+ *
+ */
+static cmdBlock cmdRegistry[] =
+{
+    { "cd|chdir", "Changes the current directory", changedir, help_cd },
+    { "copy", "Copies files and directories", doCopy, help_copy },
+    { "del", "Deletes files", dodel, help_del },
+    { "dir", "Lists files and directories", dodir, help_dir },
+    { "echo", "Displays a message", doEcho, help_echo },
+    { "exit", "Exits PCOMM", doExit, help_exit },
+    { "help", "Provides information about PDOS commands", dohelp, help_help },
+    { "mcd", "Make new directory and change to it", doMCD, help_mcd },
+    { "md|mkdir", "Creates new directories", domkdir, help_md },
+    { "path", "Displays or modifies PATH variable", dopath, help_path },
+    { "prompt", "Displays or modifies PROMPT variable", doPrompt, help_prompt },
+    { "rd|rmdir", "Removes directories", dormdir, help_rd },
+    { "reboot", "Reboots the computer", doReboot, help_reboot },
+    { "ren|rename", "Renames files and directories", dorename, help_ren },
+    { "type", "Reads and displays a text file", dotype, help_type },
+    { "ver", "Displays the current version of PDOS", dover, help_ver },
+    { NULL, NULL, NULL, NULL } /* Sentinel record for end of command registry */
+};
+
+/* Searches command registry for command with given name */
+cmdBlock *findCommand(char *cmdName)
+{
+    char tmp[128];
+    char *p1, *p2;
+    int i;
+    cmdBlock *block;
+
+    /* Ignore initial whitespace */
+    while (isspace(*cmdName))
+    {
+        cmdName++;
+    }
+
+    /* Ignore final whitespace */
+    for (i = 0; cmdName[i] != 0; i++)
+    {
+        if (isspace(cmdName[i]))
+        {
+            cmdName[i] = 0;
+            break;
+        }
+    }
+
+    /* Search the registry for the command */
+    for (i = 0; cmdRegistry[i].name != NULL; i++)
+    {
+        block = &(cmdRegistry[i]);
+        if (strchr(block->name,'|') == NULL)
+        {
+            if (ins_strcmp(block->name,cmdName) == 0)
+            {
+                return block;
+            }
+        }
+        else
+        {
+            strcpy(tmp,block->name);
+            p1 = tmp;
+            while (1)
+            {
+                p2 = strchr(p1,'|');
+                if (p2 == NULL)
+                {
+                    if (ins_strcmp(p1,cmdName) == 0)
+                    {
+                        return block;
+                    }
+                    break;
+                }
+                *p2 = 0;
+                p2++;
+                if (ins_strcmp(p1,cmdName) == 0)
+                {
+                    return block;
+                }
+                p1 = p2;
+            }
+        }
+    }
+    return NULL;
+}
 
 int main(int argc, char **argv)
 {
@@ -136,6 +277,7 @@ static void parseArgs(int argc, char **argv)
 static void processInput(void)
 {
     char *p;
+    cmdBlock *block;
 
     len = strlen(buf);
     if ((len > 0) && (buf[len - 1] == '\n'))
@@ -153,41 +295,9 @@ static void processInput(void)
         p = buf + len;
     }
     len -= (size_t)(p - buf);
-    if (ins_strcmp(buf, "exit") == 0)
-    {
-#ifdef CONTINUOUS_LOOP
-        primary = 0;
-#endif
-        if (!primary)
-        {
-            term = 1;
-        }
-    }
-    else if (ins_strcmp(buf, "path") == 0)
-    {
-        dopath(p);
-    }
-    else if (ins_strcmp(buf, "type") == 0)
-    {
-        dotype(p);
-    }
-    else if (ins_strcmp(buf, "dir") == 0)
-    {
-        dodir(p);
-    }
-    else if (ins_strcmp(buf, "ver") == 0)
-    {
-        dover();
-    }
-    else if (ins_strcmp(buf, "echo") == 0)
-    {
-        printf("%s\n", p);
-    }
-    else if (ins_strcmp(buf, "chdir") == 0 || ins_strcmp(buf, "cd") == 0)
-    {
-        changedir(p);
-    }
-    else if (ins_strncmp(buf, "cd.", 3) == 0)
+
+    /* Check for special case syntax first */
+    if (ins_strncmp(buf, "cd.", 3) == 0)
     {
         changedir(buf + 2);
     }
@@ -195,43 +305,31 @@ static void processInput(void)
     {
         changedir(buf + 2);
     }
-    else if (ins_strcmp(buf, "reboot") == 0)
-    {
-        PosReboot();
-    }
-    else if(ins_strcmp(buf, "del")==0)
-    {
-        dodel(p);
-    }
     else if ((strlen(buf) == 2) && (buf[1] == ':'))
     {
         changedisk(buf[0]);
     }
-    else if (ins_strcmp(buf, "copy") == 0)
-    {
-        doCopy(p);
-    }
-    else if (ins_strcmp(buf, "help") == 0)
-    {
-        dohelp(p);
-    }
-    else if (ins_strcmp(buf, "mkdir") == 0 || ins_strcmp(buf, "md") == 0)
-    {
-        domkdir(p);
-    }
-    else if (ins_strcmp(buf, "rmdir") == 0 || ins_strcmp(buf, "rd") == 0)
-    {
-        dormdir(p);
-    }
-    else if (ins_strcmp(buf, "rename") == 0 || ins_strcmp(buf, "ren") == 0)
-    {
-        dorename(p);
-    }
     else
     {
-        doExec(buf,p);
+        /* Look for internal command in registry */
+        block = findCommand(buf);
+        if (block != NULL)
+        {
+            /* Command was found, call implementation if present */
+            if (block->proc == NULL)
+            {
+                printf("ERROR: No implementation found for command '%s'\n", buf);
+            }
+            else
+            {
+                block->proc(p);
+            }
+        }
+        else
+        {
+            doExec(buf,p);
+        }
     }
-    return;
 }
 
 static void putPrompt(void)
@@ -244,6 +342,27 @@ static void putPrompt(void)
     printf("\n%s:\\%s%s", drive, cwd, prompt);
     fflush(stdout);
     return;
+}
+
+static void doExit(char *ignored)
+{
+#ifdef CONTINUOUS_LOOP
+    primary = 0;
+#endif
+    if (!primary)
+    {
+        term = 1;
+    }
+}
+
+static void doEcho(char *msg)
+{
+    printf("%s\n", msg);
+}
+
+static void doReboot(char *ignored)
+{
+    PosReboot();
 }
 
 static void dotype(char *file)
@@ -339,7 +458,7 @@ static void dodir(char *pattern)
     return;
 }
 
-static void dover(void)
+static void dover(char *ignored)
 {
     printf("%s %s\n", __DATE__, __TIME__);
     return;
@@ -380,6 +499,19 @@ static void dopath(char *s)
         strcat(path, s);
      }
      return;
+}
+
+static void doPrompt(char *s)
+{
+     if (strcmp(s, "") == 0)
+     {
+        printf("Current PROMPT is %s\n", prompt);
+     }
+
+     else
+     {
+        strcpy(prompt, s);
+     }
 }
 
 static void doExec(char *b,char *p)
@@ -450,7 +582,7 @@ static void doExec(char *b,char *p)
 
     if (*s == '\0')
     {
-        printf("command not found\n");
+        printf("Command '%s' not found\n", b);
     }
 
     return;
@@ -529,126 +661,216 @@ static void changedisk(int drive)
     return;
 }
 
-/* dohelp function and condition for it written by Alica Okano */
+/*
+ * **** HELP ROUTINES ***
+ */
+static void help_exit(void)
+{
+    printf("EXIT\n");
+}
+
+static void help_path(void)
+{
+    printf("PATH variable provides a list of directories\n");
+    printf("which are searched when looking for executables.\n\n");
+    printf("PATH\n");
+    printf("PATH [path]\n");
+    printf("PATH [;]\n");
+}
+
+static void help_prompt(void)
+{
+    printf("PROMPT variable stores the PCOMM prompt.\n");
+    printf("PROMPT\n");
+    printf("PROMPT [prompt]\n");
+}
+
+static void help_type(void)
+{
+    printf("TYPE [path]\n");
+}
+
+static void help_dir(void)
+{
+    printf("DIR\n");
+}
+
+static void help_ver(void)
+{
+    printf("VER\n");
+}
+
+static void help_echo(void)
+{
+    printf("ECHO [message]\n");
+}
+
+static void help_cd(void)
+{
+    printf("CD [path]\n");
+}
+
+static void help_reboot(void)
+{
+    printf("REBOOT\n");
+}
+
+static void help_del(void)
+{
+    printf("DEL [path]\n");
+}
+
+static void help_copy(void)
+{
+    printf("COPY [path1] [path2]\n");
+}
+
+static void help_help(void)
+{
+    printf("HELP\n");
+    printf("HELP [command]\n");
+}
+
+static void help_md(void)
+{
+    printf("MD [path]\n");
+}
+
+static void help_mcd(void)
+{
+    printf("MCD [path]\n");
+}
+
+static void help_rd(void)
+{
+    printf("RD [path]\n");
+}
+
+static void help_ren(void)
+{
+    printf("REN [path] [name]\n");
+}
+
+/* Modify string in-place to be all upper case */
+static void strtoupper(char *s)
+{
+    while (*s != 0)
+    {
+        *s = toupper(*s);
+        s++;
+    }
+}
+
+/* dohelp function and condition for it originally written by Alica Okano.
+ * Rewritten to use table-driven design by Simon Kissane.
+ */
 static void dohelp(char *cmd)
 {
+    int i = 0;
+    int maxCmdNameLen = 0, curCmdNameLen = 0;
+    cmdBlock *block = NULL;
+    char *pipe = NULL;
+    char tmp[128];
     if(*cmd == '\0')
     {
         printf("Use HELP [command] to obtain more information about\n");
         printf("specific command.\n\n");
-        printf("CD      Changes the current directory.\n");
-        printf("CHDIR   Changes the current directory.\n");
-        printf("COPY    Copies files and directories.\n");
-        printf("DEL     Deletes files.\n");
-        printf("DIR     Displays a list of files and directories\n");
-        printf("        in the current directory.\n");
-        printf("ECHO    Displays a message.\n");
-        printf("HELP    Provides information about PDOS commands.\n");
-        printf("MD      Creates new directories.\n");
-        printf("MKDIR   Creates new directories.\n");
-        printf("PATH    Displays or modifies PATH variable.\n");
-        printf("RD      Removes directories.\n");
-        printf("REBOOT  Reboots the computer.\n");
-        printf("RD      Removes directories.\n");
-        printf("RMDIR   Removes directories.\n");
-        printf("REN     Renames files and directories.\n");
-        printf("RENAME  Renames files and directories.\n");
-        printf("TYPE    Reads and displays a text file.\n");
-        printf("VER     Displays the current version of PDOS.\n");
-        return;
-    }
 
-    else if(ins_strcmp(cmd, "chdir") == 0 || ins_strcmp(cmd, "cd") == 0)
-    {
-        printf("Changes the current directory.\n\n");
-        printf("CHDIR [path]\n");
-        printf("CD [path]\n");
-    }
+        /* Find max command name length. Used to construct columns. */
+        for (i = 0; cmdRegistry[i].name != NULL; i++)
+        {
+            block = &(cmdRegistry[i]);
+            curCmdNameLen = strlen(block->name);
+            if (curCmdNameLen > maxCmdNameLen)
+                maxCmdNameLen = curCmdNameLen;
+        }
 
-    else if(ins_strcmp(cmd, "copy") == 0)
-    {
-        printf("Copies files and directories.\n\n");
-        printf("COPY [path1] [path2]\n");
-    }
+        /* Print each line */
+        for (i = 0; cmdRegistry[i].name != NULL; i++)
+        {
+            block = &(cmdRegistry[i]);
 
-    else if(ins_strcmp(cmd, "del") == 0)
-    {
-        printf("Deletes files.\n\n");
-        printf("DEL [path]\n");
-    }
+            /* Allocate temporary buffer to modify name */
+            memset(tmp, 0, 128);
+            strcpy(tmp,block->name);
 
-    else if(ins_strcmp(cmd, "dir") == 0)
-    {
-        printf("Displays a list of files and directories\n");
-        printf("in the current directory.\n\n");
-        printf("DIR\n");
-    }
+            /* Strip aliases */
+            pipe = strchr(tmp,'|');
+            if (pipe != NULL)
+            {
+                while (*pipe != 0)
+                {
+                    *pipe = 0;
+                    pipe++;
+                }
+            }
 
-    else if(ins_strcmp(cmd, "echo") == 0)
-    {
-        printf("Displays a message.\n\n");
-        printf("ECHO [message]\n");
-    }
+            /* Make upper case */
+            strtoupper(tmp);
 
-    else if(ins_strcmp(cmd, "help") == 0)
-    {
-        printf("Provides information about PCOMM commands.\n\n");
-        printf("HELP\n");
-        printf("HELP [command]\n");
-    }
+            /* Left pad with spaces */
+            while (strlen(tmp) < maxCmdNameLen)
+                tmp[strlen(tmp)] = ' ';
+            tmp[strlen(tmp)] = ' ';
 
-    else if(ins_strcmp(cmd, "mkdir") == 0 || ins_strcmp(cmd, "md") == 0)
-    {
-        printf("Creates new directories.\n\n");
-        printf("MKDIR [path]\n");
-        printf("MD [path]\n");
+            /* Print help line */
+            printf("%s %s.\n", tmp, block->description);
+        }
     }
-
-    else if(ins_strcmp(cmd, "path") == 0)
-    {
-        printf("Displays or modifies PATH variable.\n\n");
-        printf("PATH variable provides a list of directories\n");
-        printf("which are searched when looking for executables.\n\n");
-        printf("PATH\n");
-        printf("PATH [path]\n");
-        printf("PATH [;]\n");
-    }
-
-    else if(ins_strcmp(cmd, "reboot") == 0)
-    {
-        printf("Reboots the computer.\n\n");
-        printf("REBOOT\n");
-    }
-
-    else if(ins_strcmp(cmd, "rmdir") == 0 || ins_strcmp(cmd, "rd") == 0)
-    {
-        printf("Removes directories.\n\n");
-        printf("RMDIR [path]\n");
-        printf("RD [path]\n");
-    }
-
-    else if(ins_strcmp(cmd, "rename") == 0 || ins_strcmp(cmd, "ren") == 0)
-    {
-        printf("Renames files and directories.\n\n");
-        printf("RENAME [path] [name]\n");
-        printf("REN [path] [name]\n");
-    }
-
-    else if(ins_strcmp(cmd, "type") == 0)
-    {
-        printf("Reads and displays a text file.\n\n");
-        printf("TYPE [path]\n");
-    }
-
-    else if(ins_strcmp(cmd, "ver") == 0)
-    {
-        printf("Displays the current version of PDOS.\n\n");
-        printf("VER\n");
-    }
-
     else
     {
-        printf("No such command \"%s\"\n",cmd);
+        /* Lookup command, report error if not found */
+        block = findCommand(cmd);
+        if (block == NULL)
+        {
+            printf("ERROR: No such command \"%s\"\n",cmd);
+            return;
+        }
+
+        /* Allocate temporary buffer to modify name */
+        memset(tmp, 0, 128);
+        strcpy(tmp,block->name);
+
+        /* Strip aliases */
+        pipe = strchr(tmp,'|');
+        if (pipe != NULL)
+        {
+            while (*pipe != 0)
+            {
+                *pipe = 0;
+                pipe++;
+            }
+        }
+
+        /* Make upper case */
+        strtoupper(tmp);
+
+        /* Print header line */
+        printf("%s - %s\n\n", tmp, (block->description != NULL ? block->description : "(no description)"));
+
+        /* Print aliases */
+        if (strchr(block->name,'|') != NULL)
+        {
+            memset(tmp, 0, 128);
+            strcpy(tmp,block->name);
+            pipe = strchr(tmp,'|');
+            pipe++;
+            for (i = 0; pipe[i] != 0; i++)
+            {
+                pipe[i] = toupper(pipe[i]);
+                if (pipe[i] == '|')
+                {
+                    pipe[i] = ' ';
+                }
+            }
+            printf("Aliases: %s\n\n", pipe);
+        }
+
+        /* Print detailed help */
+        if (block->help != NULL)
+        {
+            (block->help)();
+        }
     }
 
     return;
@@ -670,6 +892,11 @@ static void domkdir(char *dnm)
     if (ret == POS_ERR_ACCESS_DENIED) printf("Access denied\n");
 
     return;
+}
+
+static void doMCD(char *dnm) {
+    domkdir(dnm);
+    changedir(dnm);
 }
 
 static void dormdir(char *dnm)

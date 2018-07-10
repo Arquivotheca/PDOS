591d1faa6891501d338f4dde45a08c79afec5a3b
Paul Edwards <mutazilah@gmail.com>
2018-07-10 17:46:37 +1000

    do a build using Watcom

---
+++ src/comp4w.bat
@@ -0,0 +1,32 @@
+@echo on
+wasm -zq -zcm -Dmemodel=tiny support.asm 
+wasm -zq -zcm -Dmemodel=tiny -DWATCOM ploadst.asm
+wasm -zq -zcm -Dmemodel=tiny -DWATCOM nearw.asm
+wasm -zq -zcm -Dmemodel=tiny lldos.asm
+wasm -zq -zcm -Dmemodel=tiny protinta.asm
+wasm -zq -zcm -Dmemodel=tiny int13x.asm
+wcl -ecc -q -w -c -I. -mt -zl -D__MSDOS__ -fpi87 -s -zdp -zu -DPDOS32 -I..\pdpclib pload.c protint.c file.c fat.c bos.c
+wcl -ecc -q -w -c -I. -mt -zl -D__MSDOS__ -fpi87 -s -zdp -zu -DPDOS32 -DNEED_DUMP -I..\pdpclib pdosload.c
+wcl -ecc -q -w -c -I. -mt -zl -D__MSDOS__ -fpi87 -s -zdp -zu -I..\pdpclib ..\pdpclib\string.c ..\pdpclib\dossupc.c
+wcl -ecc -q -w -c -I. -mt -zl -D__MSDOS__ -fpi87 -s -zdp -zu -I..\pdpclib ..\pdpclib\ctype.c
+if exist watcom.lib del watcom.lib
+wlib -b -q watcom +support.obj
+wlib -b -q watcom +bos.obj
+wlib -b -q watcom +dossupc.obj
+wlib -b -q watcom +fat.obj
+wlib -b -q watcom +string.obj
+wlib -b -q watcom +pdosload.obj
+wlib -b -q watcom +lldos.obj
+wlib -b -q watcom +protinta.obj
+wlib -b -q watcom +file.obj
+wlib -b -q watcom +protint.obj
+wlib -b -q watcom +ctype.obj
+
+rem the order of the following stuff is critical so that the first 3*512
+rem bytes have sufficient stuff to do the rest of the load of itself!!!
+rem it is ploadst, pload, int13x and near that need to be in the first 3*512
+rem and only the first 3 functions of near
+rem wcl ploadst.obj pload.obj
+rem wlink ploadst+pload+int13x+near+support+bos+dossupc+fat+string+pdosload+lldos+protinta+file+protint,pload.com,,,
+
+wlink File ploadst.obj,pload.obj,int13x.obj,nearw.obj Name pload.com Form dos com Library watcom.lib Option map

540a5bebee9fc95b981b1f9059ebcd11e1243727
Paul Edwards <mutazilah@gmail.com>
2018-07-11 10:24:43 +1000

    fixups required for near vs far pointers

--- src/nearw.asm
+++ src/nearw.asm
@@ -45,24 +45,24 @@ push bp
 mov bp,sp
 push bx
 
-cmp word ptr [bp + 12], 0
+cmp word ptr [bp + 10], 0
 jne ludiv_full
 
-mov ax, [bp + 8]
-mov dx, 0
-div word ptr [bp + 10]
-mov bx, ax
 mov ax, [bp + 6]
-div word ptr [bp + 10]
+mov dx, 0
+div word ptr [bp + 8]
+mov bx, ax
+mov ax, [bp + 4]
+div word ptr [bp + 8]
 
 mov dx, bx
 jmp short ludiv_fin
 
 ludiv_full:
-push word ptr [bp + 12]
 push word ptr [bp + 10]
 push word ptr [bp + 8]
 push word ptr [bp + 6]
+push word ptr [bp + 4]
 ;call far ptr ___divide
 add sp, 8
 
@@ -80,23 +80,23 @@ f_lumod@ proc
 push bp
 mov bp,sp
 
-cmp word ptr [bp + 12], 0
+cmp word ptr [bp + 10], 0
 jne lumod_full
 
-mov ax, [bp + 8]
-mov dx, 0
-div word ptr [bp + 10]
 mov ax, [bp + 6]
-div word ptr [bp + 10]
+mov dx, 0
+div word ptr [bp + 8]
+mov ax, [bp + 4]
+div word ptr [bp + 8]
 mov ax,dx
 mov dx, 0
 jmp short lumod_fin
 
 lumod_full:
-push word ptr [bp + 12]
 push word ptr [bp + 10]
 push word ptr [bp + 8]
 push word ptr [bp + 6]
+push word ptr [bp + 4]
 ;call far ptr ___modulo
 add sp, 8
 

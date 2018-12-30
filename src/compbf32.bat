cc92e90fc13eeee3b85012e99f063366b7b8e26a
Paul Edwards <mutazilah@gmail.com>
2018-12-30 15:53:01 +1100

    allow assembly with tasm

--- src/compbf32.bat
+++ src/compbf32.bat
@@ -1,3 +1,4 @@
 @echo off
-wasmr -zq -zcm -Dmemodel=tiny fat32.asm
-tlink -t -x fat32,fat32.com,,,
+wasm -zq -zcm -Dmemodel=tiny fat32.asm
+rem tasm -m2 -Dmemodel=tiny fat32.asm
+rem tlink -t -x fat32,fat32.com,,,

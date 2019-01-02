bce24d969604363a953e2ed9c04eb7e0f4fcf5c8
Paul Edwards <mutazilah@gmail.com>
2019-01-02 15:07:34 +1100

    allow entry point to work with Watcom

--- pdpclib/pdosst32.c
+++ pdpclib/pdosst32.c
@@ -6,7 +6,13 @@
    a C program.  Do not put any string literals in here though,
    as the code needs to start immediately.  This is IT!  */
 
-int __pdosst32(int i1, int i2, int i3, void *exep)
+#ifdef __WATCOMC__
+#define CTYP __cdecl
+#else
+#define CTYP
+#endif
+
+int CTYP __pdosst32(int i1, int i2, int i3, void *exep)
 {
     int rc;
 

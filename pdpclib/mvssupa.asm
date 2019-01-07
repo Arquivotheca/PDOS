371d2e80a160f0364a0f528f67ed8aa647e50e8e
Paul Edwards <mutazilah@gmail.com>
2019-01-07 13:27:53 +1100

    allow lengths greater than 2 GiB in GETM

--- pdpclib/mvssupa.asm
+++ pdpclib/mvssupa.asm
@@ -3232,7 +3232,7 @@ TRUNCOEX L     R13,4(,R13)
          LDINT R3,0(,R1)          LOAD REQUESTED STORAGE SIZE
          SLR   R5,R5              PRESET IN CASE OF ERROR       GP15017
          LTR   R4,R3              CHECK REQUEST
-         BNP   GETMEX             QUIT IF INVALID
+*        BNP   GETMEX             QUIT IF INVALID
 *
 * To reduce fragmentation, round up size to 64 byte multiple
 *

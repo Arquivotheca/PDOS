74e648fe21d0ecc6def19f1a3a789b60088e558c
Paul Edwards <mutazilah@gmail.com>
2019-01-10 03:33:09 +1100

    put back line for MSDOS compilation using wasmr

--- src/compbf32.bat
+++ src/compbf32.bat
@@ -1,4 +1,5 @@
-wasm -zq -zcm -Dmemodel=tiny fat32.asm
+rem wasmr -zq -zcm -Dmemodel=tiny fat32.asm
 rem tasm -m2 -Dmemodel=tiny fat32.asm
 rem tlink -t -x fat32,fat32.com,,,
+wasm -zq -zcm -Dmemodel=tiny fat32.asm
 wlink File fat32.obj Name fat32.com Form dos com Option map

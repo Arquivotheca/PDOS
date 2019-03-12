2f12a7829c2b942651c0bbca7e127126df3c2a4f
Paul Edwards <mutazilah@gmail.com>
2019-03-12 16:00:52 +1100

    insert cursor at input field

--- s370/pdos.c
+++ s370/pdos.c
@@ -2948,7 +2948,7 @@ static void join_cchhr(char *cchhr, int cyl, int head, int rec)
 
 static void write3270(char *buf, size_t lenbuf, int cr)
 {
-    static char intbuf[6+22*80+2];
+    static char intbuf[6+22*80+3];
     static int first = 1;
     static int lineupto = 0;
 
@@ -2957,7 +2957,7 @@ static void write3270(char *buf, size_t lenbuf, int cr)
         first = 0;
         memset(intbuf, ' ', sizeof intbuf);
         memcpy(intbuf, "\xc3\x11\x5d\x7f\x1d\xf0", 6);
-        memcpy(intbuf + 6 + 22 * 80, "\x1d\x00", 2);
+        memcpy(intbuf + 6 + 22 * 80, "\x1d\x00\x13", 3);
     }
     memset(intbuf + 6 + lineupto * 80, ' ', 80);
     memcpy(intbuf + 6 + lineupto * 80, buf, lenbuf);

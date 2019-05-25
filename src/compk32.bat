62f93c043d50a8b908a3eaa9f874655ae543f610
Paul Edwards <mutazilah@gmail.com>
2019-05-26 06:59:27 +1000

    mod from Alica to eliminate need for kernel32.def

--- src/compk32.bat
+++ src/compk32.bat
@@ -2,5 +2,5 @@ gcc -c -O0 -D__WIN32__ -I ../pdpclib -I . kernel32.c
 gcc -c -O0 -D__WIN32__ -D__32BIT__ -I . pos.c
 gcc -c -O0 -D__WIN32__ -I . support.s
 gcc -shared -s -nostdlib -o kernel32.dll -Wl,--kill-at kernel32.o pos.o support.o
-dlltool -k -d kernel32.def -l libkernel32.a
+dlltool -k --export-all-symbols -D kernel32.dll kernel32.o -l libkernel32.a
 

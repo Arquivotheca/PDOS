57a772c4a796c46b1be7be8865c40b2f920ee47f
Paul Edwards <mutazilah@gmail.com>
2019-06-14 07:04:45 +1000

    mod from Alica to build msvcrt.dll

---
+++ pdpclib/compmsvcrt.bat
@@ -0,0 +1,16 @@
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . dllcrt.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . stdio.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . string.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . stdlib.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . start.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . time.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . errno.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . assert.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . signal.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . locale.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . ctype.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . setjmp.c
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . math.c
+jwasm -c -coff winsupa.asm
+gcc -shared -s -nostdlib -o msvcrt.dll dllcrt.o stdio.o string.o stdlib.o start.o time.o errno.o assert.o signal.o locale.o ctype.o setjmp.o math.o winsupa.obj -lkernel32
+dlltool -D msvcrt.dll stdio.o string.o stdlib.o start.o time.o errno.o assert.o signal.o locale.o ctype.o setjmp.o math.o -l libmsvcrt.a

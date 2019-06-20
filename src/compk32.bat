9c98f33f93b7a01ca7e571ac0a39ea6b3d551236
Paul Edwards <mutazilah@gmail.com>
2019-06-21 02:15:38 +1000

    whitespace

--- src/compk32.bat
+++ src/compk32.bat
@@ -4,4 +4,3 @@ gcc -c -O0 -D__WIN32__ -D__32BIT__ -I . pos.c
 gcc -c -O0 -D__WIN32__ -I . support.s
 gcc -shared -s -nostdlib -o kernel32.dll -Wl,--kill-at dllcrt.o kernel32.o pos.o support.o
 dlltool -k --export-all-symbols -D kernel32.dll kernel32.o -l libkernel32.a
-

a10c2e3ff41537fac65dc757de730d5110ef555a
Paul Edwards <mutazilah@gmail.com>
2020-04-02 16:03:10 +1100

    switch to gccwin etc

--- src/compk32.bat
+++ src/compk32.bat
@@ -1,6 +1,17 @@
-gcc -c -O0 -D__WIN32__ -I ../pdpclib -I . ../pdpclib/dllcrt.c
-gcc -c -O0 -D__WIN32__ -I ../pdpclib -I . kernel32.c
-gcc -c -O0 -D__WIN32__ -D__32BIT__ -I . pos.c
-gcc -c -O0 -D__WIN32__ -I . support.s
-gcc -shared -s -nostdlib -o kernel32.dll -Wl,--kill-at dllcrt.o kernel32.o pos.o support.o
+gccwin -S -Os -D__WIN32__ -I ../pdpclib -I . ../pdpclib/dllcrt.c
+aswin -o dllcrt.o dllcrt.s
+del dllcrt.s
+
+gccwin -S -Os -D__WIN32__ -I ../pdpclib -I . kernel32.c
+aswin -o kernel32.o kernel32.s
+del kernel32.s
+
+gccwin -S -Os -D__WIN32__ -D__32BIT__ -I ../pdpclib -I . pos.c
+aswin -o pos.o pos.s
+del pos.s
+
+aswin -o support.o support.s
+
+ldwin -s -o kernel32.dll --shared --kill-at dllcrt.o kernel32.o pos.o support.o
+
 dlltool -k --export-all-symbols -D kernel32.dll kernel32.o -l libkernel32.a

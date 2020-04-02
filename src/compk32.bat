e824bc6a453a45b51d476fb35595d292cb1c20a0
Paul Edwards <mutazilah@gmail.com>
2020-04-02 21:56:23 +1100

    compile kernel32 with Cygwin

--- src/compk32.bat
+++ src/compk32.bat
@@ -6,6 +6,10 @@ gccwin -S -Os -D__WIN32__ -I ../pdpclib -I . kernel32.c
 aswin -o kernel32.o kernel32.s
 del kernel32.s
 
+rem for some reason gccwin or aswin are not generating the
+rem correct object code, so we override with gcc 4.8.2 from Cygwin
+gcc -nostdinc -c -Os -D__WIN32__ -I ../pdpclib -I . kernel32.c
+
 gccwin -S -Os -D__WIN32__ -D__32BIT__ -I ../pdpclib -I . pos.c
 aswin -o pos.o pos.s
 del pos.s

c219bb6cf65731adde1681aeebbb4e1cdb2b7506
Paul Edwards <mutazilah@gmail.com>
2020-10-11 06:43:16 +1100

    document -m2 for tasm

--- src/compb.bat
+++ src/compb.bat
@@ -1,4 +1,6 @@
 @echo off
 wasmr -zq -zcm -Dmemodel=tiny pbootsec.asm
+rem tasm requires 2 passes to generate the desired near jump
+rem for first instruction
 rem tasm -m2 -Dmemodel=tiny pbootsec.asm
 tlink -t -x pbootsec,pbootsec.com,,,

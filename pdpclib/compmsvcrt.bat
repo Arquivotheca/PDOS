24c718fea2ca1cfb6f53146b2369be79d2e39e81
Paul Edwards <mutazilah@gmail.com>
2020-10-25 09:15:24 +1100

    trim down long line

--- pdpclib/compmsvcrt.bat
+++ pdpclib/compmsvcrt.bat
@@ -13,4 +13,7 @@ gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . -I../src setjmp.c
 gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . -I../src math.c
 jwasm -c -coff winsupa.asm
 gcc -shared -s -nostdlib -o msvcrt.dll dllcrt.o stdio.o string.o stdlib.o start.o time.o errno.o assert.o signal.o locale.o ctype.o setjmp.o math.o winsupa.obj ../src/kernel32.a
-dlltool -D msvcrt.dll stdio.o string.o stdlib.o start.o time.o errno.o assert.o signal.o locale.o ctype.o setjmp.o math.o -l msvcrt.a
+echo stdio.o string.o stdlib.o start.o time.o errno.o >dlltool.in
+echo assert.o signal.o locale.o ctype.o setjmp.o math.o >>dlltool.in
+dlltool -D msvcrt.dll -l msvcrt.a @dlltool.in
+del dlltool.in

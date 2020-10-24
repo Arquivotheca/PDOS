2ed1b9b986feacc4c7e62bfa35164fb884b067de
Paul Edwards <mutazilah@gmail.com>
2020-10-24 19:13:53 +1100

    put dllcrt back in as otherwise DLL doesn't work

--- pdpclib/compmsvcrt.bat
+++ pdpclib/compmsvcrt.bat
@@ -1,3 +1,4 @@
+gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . dllcrt.c
 gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . stdio.c
 gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . string.c
 gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . stdlib.c
@@ -11,5 +12,5 @@ gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . ctype.c
 gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . setjmp.c
 gcc -c -O2 -D__WIN32__ -D__PDPCLIB_DLL -I . math.c
 jwasm -c -coff winsupa.asm
-gcc -shared -s -nostdlib -o msvcrt.dll -Wl,--kill-at stdio.o string.o stdlib.o start.o time.o errno.o assert.o signal.o locale.o ctype.o setjmp.o math.o winsupa.obj -lkernel32
+gcc -shared -s -nostdlib -o msvcrt.dll -Wl,--kill-at dllcrt.o stdio.o string.o stdlib.o start.o time.o errno.o assert.o signal.o locale.o ctype.o setjmp.o math.o winsupa.obj -lkernel32 -L ../src
 dlltool -k -D msvcrt.dll stdio.o string.o stdlib.o start.o time.o errno.o assert.o signal.o locale.o ctype.o setjmp.o math.o -l libmsvcrt.a

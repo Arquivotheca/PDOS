3bf51e1c5348e1585bfbf419698cf21f1fc6c937
Paul Edwards <mutazilah@gmail.com>
2021-03-09 09:27:56 +1100

    more Amiga functions

--- amiga/clib/dos_protos.h
+++ amiga/clib/dos_protos.h
@@ -1,4 +1,10 @@
 /* public domain by Paul Edwards */
 
+#define MODE_OLDFILE 1005
+#define MODE_NEWFILE 1006
+
+void *Input(void);
+void *Output(void);
+void *Open(const char *fnm, long b);
 long Read(void *a, void *b, long c);
 long Write(void *a, void *b, long c);

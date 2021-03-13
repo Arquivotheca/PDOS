0a152487d8bf808ebbcc9c2c471edaa5b189b173
Paul Edwards <mutazilah@gmail.com>
2021-03-14 02:28:32 +1100

    remove junk code and fix comments

--- pdpclib/amisupa.asm
+++ pdpclib/amisupa.asm
@@ -6,29 +6,15 @@
 ***********************************************************************
 ***********************************************************************
 *                                                                     *
-*  amistart - startup code for AmigaOS.                               *
+*  amisupa - support routines for AmigaOS.                            *
 *                                                                     *
 *  This uses the Motorola syntax                                      *
-*                                                                     *
-*  It puts the standard AmigaOS registers containing the command      *
-*  length (d0) and command buffer (a0) as well as the AmigaPDOS       *
-*  extension register (only visible if running AmigaPDOS) a6,         *
-*  containing an alternative SysBase to use (only if d0 is greater    *
-*  than or equal to 2 GiB, and in which case, 2 GiB should be         *
-*  subtracted from d0 before use) on the stack.                       *
-*                                                                     *
-*  All this manipulation is left for the C startup code to handle.    *
 *                                                                     *
 ***********************************************************************
 *
         section "CODE",code
         xdef ___setj
         xdef ___longj
-
-___amistart:
-        movem.l d0/a0/a6,-(sp)
-        jsr ___start
-        rts
 
 * These need to be implemented
 

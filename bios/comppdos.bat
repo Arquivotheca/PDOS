7b19375d2f8bc5e01a30351be65cbd84373ddefa
Paul Edwards <mutazilah@gmail.com>
2021-03-19 20:12:17 +1100

    build 80386 a.out version of OS

---
+++ bios/comppdos.bat
@@ -0,0 +1,18 @@
+del *.o
+del osworld.exe
+
+gcc386 -S -Os -fno-common -I. -o osworld.s osworld.c
+as386 -o osworld.o osworld.s
+del osworld.s
+
+gcc386 -S -Os -fno-common -I. -o genstart.s genstart.c
+as386 -o genstart.o genstart.s
+del genstart.s
+
+gcc386 -S -Os -fno-common -I. -o osfunc.s osfunc.c
+as386 -o osfunc.o osfunc.s
+del osfunc.s
+
+ld386 -s -e ___crt0 -o osworld.exe genstart.o osworld.o osfunc.o
+ld386 -r -s -e ___crt0 -o osworld.exe genstart.o osworld.o osfunc.o
+strip386 --strip-unneeded osworld.exe

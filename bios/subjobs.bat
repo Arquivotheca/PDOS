3ee0ea3ff9abffa2091ce0bc9f076806c2fdfaea
Paul Edwards <mutazilah@gmail.com>
2021-03-19 19:58:44 +1100

    batch files to build on MVS

---
+++ bios/subjobs.bat
@@ -0,0 +1,8 @@
+del alljcl.jcl
+type bios1.jcl >>alljcl.jcl
+type bios2.jcl >>alljcl.jcl
+type bios3.jcl >>alljcl.jcl
+type bios4.jcl >>alljcl.jcl
+type bios5.jcl >>alljcl.jcl
+type bios6.jcl >>alljcl.jcl
+call runmvs alljcl.jcl output.txt all.zip
---
+++ bios/zipmvs.bat
@@ -0,0 +1,10 @@
+del biosinc.zip
+del biossrc.zip
+del biosjcl.zip
+del all.zip
+del alljcl.jcl
+del output.txt
+zip -9 -X -ll -j biossrc.zip *.c
+zip -9 -X -ll -j biosinc.zip *.h
+zip -9 -X -ll -j biosjcl.zip *.jcl
+zip -9 -X all *.zip

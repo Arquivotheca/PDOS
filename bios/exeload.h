e3fa454c7cd984f2ae6d5ad44450db7c3700d2ce
Paul Edwards <mutazilah@gmail.com>
2021-03-19 20:07:50 +1100

    header file for exeload.c

---
+++ bios/exeload.h
@@ -0,0 +1,14 @@
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Alica Okano.                             */
+/*  Released to the Public Domain as discussed here:                 */
+/*  http://creativecommons.org/publicdomain/zero/1.0/                */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  exeload.h - header for exeload.c                                 */
+/*                                                                   */
+/*********************************************************************/
+
+int exeloadDoload(char **entry_point, char *progname, char **loadloc);

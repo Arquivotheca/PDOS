4c084067aa65d465553a6f358d82194230b3c46c
Paul Edwards <mutazilah@gmail.com>
2021-03-19 18:34:34 +1100

    add Seek

--- amiga/clib/dos_protos.h
+++ amiga/clib/dos_protos.h
@@ -3,8 +3,13 @@
 #define MODE_OLDFILE 1005
 #define MODE_NEWFILE 1006
 
+#define OFFSET_BEGINNING -1
+#define OFFSET_CURRENT 0
+#define OFFSET_END 1
+
 void *Input(void);
 void *Output(void);
 void *Open(const char *fnm, long b);
 long Read(void *a, void *b, long c);
 long Write(void *a, void *b, long c);
+long Seek(void *a, long offs, long c);

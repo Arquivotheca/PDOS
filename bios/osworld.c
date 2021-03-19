f8d27143ac17e78c1c4a1b0076beee1c1590ad96
Paul Edwards <mutazilah@gmail.com>
2021-03-19 20:05:41 +1100

    sample operating system

---
+++ bios/osworld.c
@@ -0,0 +1,28 @@
+/*********************************************************************/
+/*                                                                   */
+/*  This Program Written by Paul Edwards.                            */
+/*  Released to the Public Domain                                    */
+/*                                                                   */
+/*********************************************************************/
+/*********************************************************************/
+/*                                                                   */
+/*  osworld - example operating system that calls the generic BIOS   */
+/*                                                                   */
+/*********************************************************************/
+
+#include <osfunc.h>
+
+char x = 9;
+
+int main(void)
+{
+    /* return (x); */
+#ifdef __MVS__
+    osfunc(OS_PRINTF, 0, "\x27[2J");
+#else
+    /* osfunc(OS_PRINTF, 0, "\x1b[2J"); */
+#endif
+    osfunc(OS_PRINTF, 0, "hello, world from osworld\n");
+    osfunc(OS_PRINTF, 0, "just checking!\n");
+    return (x);
+}

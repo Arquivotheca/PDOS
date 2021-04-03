090077a8b3ecc4feff7f19ea347850dfbaeb52f8
Paul Edwards <mutazilah@gmail.com>
2021-04-03 11:16:57 +1100

    add __main

--- bios/genstart.c
+++ bios/genstart.c
@@ -15,9 +15,24 @@ extern void *__cbdata;
 
 int main(void);
 
+
+/* This name is known to certain versions of "ld" as the entry
+point of an application and there is no particular reason to not
+use it. */
+
 int __crt0(void (*exposfunc)(void *cbdata, int funccode, void *retptr, char *str), void *cbdata)
 {
     __exposfunc = exposfunc;
     __cbdata = cbdata;
     return (main());
+}
+
+
+/* Unfortunately gccwin and gcc have been built without
+HAS_INIT_SECTION defined, so this dummy function is
+required to satisfy the mandatory call from main() */
+
+int __main(void)
+{
+    return (0);
 }

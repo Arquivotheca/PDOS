3d766595a8a960469edd75f11c764ed2a20c5865
Paul Edwards <mutazilah@gmail.com>
2021-04-03 12:29:36 +1100

    compile a Win32 executable

---
+++ bios/comppd32.bat
@@ -0,0 +1,21 @@
+del *.o
+del osworld.exe
+
+gccwin -S -Os -fno-common -I. -o osworld.s osworld.c
+aswin -o osworld.o osworld.s
+del osworld.s
+
+gccwin -S -Os -fno-common -I. -o genstart.s genstart.c
+aswin -o genstart.o genstart.s
+del genstart.s
+
+gccwin -S -Os -fno-common -I. -o osfunc.s osfunc.c
+aswin -o osfunc.o osfunc.s
+del osfunc.s
+
+rem I don't know why I need to use "pie" to stop relocations
+rem from being stripped
+ld -pie -s -e ___crt0 -o osworld.exe genstart.o osworld.o osfunc.o
+
+rem Don't do this - it will strip relocations
+rem stripwin --strip-unneeded osworld.exe

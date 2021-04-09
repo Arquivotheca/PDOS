6930395c95981b5ce2c30b5a57fe27ae5b344995
Paul Edwards <mutazilah@gmail.com>
2021-04-10 08:52:19 +1000

    switch from nasm syntax to masm syntax

--- pdpclib/smlstart.asm
+++ pdpclib/smlstart.asm
@@ -4,129 +4,212 @@
 ; This program written by Paul Edwards
 ; Released to the public domain
 
-bits 16
+% .model memodel, c
 
-section .text
-	global	___intstart
-___intstart:
+.386
+
+public __intstart, __exita
+
+public __creat
+public __open
+public __close
+public __read
+public __write
+public __remove
+public __rename
+public __seek
+public __lesf2
+public __mulsf3
+public __floatsisf
+public __addsf3
+public __negsf2
+public __gesf2
+public __divsf3
+public __fixsfsi
+public __subsf3
+
+public __psp
+public __envptr
+public __osver
+
+extrn __start:proc
+
+        .code
+
+__intstart:
         mov     al, 0
         mov     ah, 4ch
         int     21h ; terminate
 
-	push	ebp
-	movzx	ebp, sp
-	mov	eax, 5
-L1:
-	db	0x66
-	leave
-	retf
-L3:
+        push    ebp
+        movzx   ebp, sp
+        mov     eax, 5
 
-        global  ___exita
-___exita:
+        db      066H
+        leave
         retf
 
 
-        global  ___creat
-___creat:
-        retf
+__exita proc, retcode: word
+        ret
+__exita endp
 
 
-        global  ___open
-___open:
-        retf
+__creat:
+__open:
+__close:
+__read:
+__write:
+__remove:
+__rename:
+__seek:
+__lesf2:
+__mulsf3:
+__floatsisf:
+__addsf3:
+__negsf2:
+__gesf2:
+__divsf3:
+__fixsfsi:
+__subsf3:
+        ret
 
 
-        global  ___close
-___close:
-        retf
+        .data
+
+banner  db  "PDPCLIB"
+__psp   dd  ?
+__envptr dd  ?
+__osver dw  ?
+
+end
 
 
-        global  ___read
-___read:
-        retf
 
 
-        global  ___write
-___write:
-        retf
+;bits 16
+
+;section .text
+;       global   ___intstart
+;___intstart:
+;        mov     al, 2
+;        mov     ah, 4ch
+;        int     21h ; terminate
+
+;       push    ebp
+;       movzx   ebp, sp
+;       mov     eax, 5
+;L1:
+;       db      0x66
+;       leave
+;       retf
+;L3:
+
+;       global  ___exita
+;___exita:
+;        retf
 
 
-        global  ___remove
-___remove:
-        retf
+;        global  ___creat
+;___creat:
+;        retf
 
 
-        global  ___rename
-___rename:
-        retf
+;        global  ___open
+;___open:
+;        retf
 
 
-        global  ___seek
-___seek:
-        retf
+;        global  ___close
+;___close:
+;        retf
 
 
-        global  ___lesf2
-___lesf2:
-        retf
+;        global  ___read
+;___read:
+;        retf
 
 
-        global  ___mulsf3
-___mulsf3:
-        retf
+;        global  ___write
+;___write:
+;        retf
 
 
-        global  ___floatsisf
-___floatsisf:
-        retf
+;        global  ___remove
+;___remove:
+;        retf
 
 
-        global  ___addsf3
-___addsf3:
-        retf
+;        global  ___rename
+;___rename:
+;        retf
 
 
-        global  ___negsf2
-___negsf2:
-        retf
+;        global  ___seek
+;___seek:
+;        retf
 
 
-        global  ___gesf2
-___gesf2:
-        retf
+;        global  ___lesf2
+;___lesf2:
+;        retf
 
 
-        global  ___divsf3
-___divsf3:
-        retf
+;        global  ___mulsf3
+;___mulsf3:
+;        retf
 
 
-        global  ___fixsfsi
-___fixsfsi:
-        retf
+;        global  ___floatsisf
+;___floatsisf:
+;        retf
 
 
-        global  ___subsf3
-___subsf3:
-        retf
+;        global  ___addsf3
+;___addsf3:
+;        retf
 
 
-extern  ___start
+;        global  ___negsf2
+;___negsf2:
+;        retf
 
-global ___psp
-global ___envptr
-global ___osver
 
-section .data
-         align 4
-banner:
-         db  "PDPCLIB"
-___psp:
+;        global  ___gesf2
+;___gesf2:
+;        retf
+
+
+;        global  ___divsf3
+;___divsf3:
+;        retf
+
+
+;        global  ___fixsfsi
+;___fixsfsi:
+;        retf
+
+
+;        global  ___subsf3
+;___subsf3:
+;        retf
+
+
+;extern  ___start
+
+;global ___psp
+;global ___envptr
+;global ___osver
+
+;section .data
+;         align 4
+;banner:
+;         db  "PDPCLIB"
+;___psp:
 ;         dd  ?
-___envptr:
+;___envptr:
 ;         dd ?
-___osver:
+;___osver:
 ;         dw ?
 
 ;top:

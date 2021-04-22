e0a935ef20fec271b1bc8b64e63d30ba823bd9eb
Paul Edwards <mutazilah@gmail.com>
2021-04-23 08:26:27 +1000

    document PDPCLIB and make link quiet

--- src/comp3w.bat
+++ src/comp3w.bat
@@ -1,3 +1,5 @@
+rem uses PDPCLIB's makefile.wcd
+
 wasm -zq -zcm -Dmemodel=large support.asm
 wcl -q -w -c -I. -ml -zl -D__MSDOS__ -fpi87 -s -zdp -zu -I..\pdpclib pcomm.c pos.c dostime.c
-wlink File pcomm.obj,pos.obj,dostime.obj,support.obj Name pcomm.exe Form dos Library ..\pdpclib\watcom.lib
+wlink File pcomm.obj,pos.obj,dostime.obj,support.obj Name pcomm.exe Form dos Library ..\pdpclib\watcom.lib Option quiet

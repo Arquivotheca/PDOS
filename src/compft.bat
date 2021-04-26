65bdecb7684ef4287ac7142c52620654f2a7b505
Paul Edwards <mutazilah@gmail.com>
2021-04-26 10:41:32 +1000

    switch to watcom

--- src/compft.bat
+++ src/compft.bat
@@ -1,4 +1,7 @@
-wasmr -zq -zcm -Dmemodel=large support.asm
-tcc -O -c -ml -I..\pdpclib functest.c pos.c bos.c
-tlink -x functest+bos+pos+support,functest.exe,,..\pdpclib\borland.lib
+rem wasmr -zq -zcm -Dmemodel=large support.asm
+rem tcc -O -c -ml -I..\pdpclib functest.c pos.c bos.c
+rem tlink -x functest+bos+pos+support,functest.exe,,..\pdpclib\borland.lib
 
+wasm -zq -zcm -Dmemodel=large support.asm
+wcl -q -w -c -I. -ml -zl -D__MSDOS__ -fpi87 -s -zdp -zu -I..\pdpclib functest.c pos.c bos.c
+wlink File functest.obj,pos.obj,bos.obj,support.obj Name functest.exe Form dos Library ..\pdpclib\watcom.lib Option quiet

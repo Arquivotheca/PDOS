17662e92df94a1491a30b209aef4051342be07ee
Paul Edwards <mutazilah@gmail.com>
2021-04-29 01:29:53 +1000

    delete obsolete comments

--- bios/comppd32.bat
+++ bios/comppd32.bat
@@ -13,6 +13,4 @@ gccwin -S -Os -fno-common -I. -o osfunc.s osfunc.c
 aswin -o osfunc.o osfunc.s
 del osfunc.s
 
-rem I don't know why I need to use "pie" to stop relocations
-rem from being stripped
 ldwin -s -e ___crt0 -o osworld.exe genstart.o osworld.o osfunc.o

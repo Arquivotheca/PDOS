b622ebed48277de7ad1b57e0aa107bbd40fb9999
Paul Edwards <mutazilah@gmail.com>
2021-06-12 10:32:14 +1000

    bug fixes

--- src/needpdos.asm
+++ src/needpdos.asm
@@ -2,11 +2,17 @@
 ;
 ; This program written by Paul Edwards
 ; Released to the public domain
+;
+; Assemble with tasm -l needpdos.asm
+; Link with tlink needpdos
 
 .model tiny
 
 _DATA   segment word public 'DATA'
-msg  db  "Please install HX or upgrade to PDOS/386 or Wine etc etc$"
+msg  db  "Install HX or upgrade to PDOS/386 or Wine etc"
+msg2 db  0DH
+msg3 db  0AH
+msg4 db "$"
 _DATA   ends
 _BSS    segment word public 'BSS'
 _BSS    ends
@@ -23,18 +29,28 @@ top:
 
 ___intstart proc
 
-mov dx,DGROUP
-mov ds,dx
+; I am suspicious about how this DGROUP gets set. Although it seems
+; to work, it appears as x'0000' in the executable, so it must be
+; dependent on being relocated. But we don't want relocations to
+; be done, as we are only changing the code, not the header. So
+; switching DS (pointing to PSP) to CS is the right thing to do.
+;mov dx,DGROUP
+;mov ds,dx
+
+push cs
+pop ds
 
 mov ah,09h
 mov dx,offset msg
 int 21h
 
-mov al,0
+mov al,1 ; set an error return code
 mov ah,4ch
 int 21h ; terminate
 
-ret
+; No need for a return. We're screwed anyway if the above
+; doesn't work. There's nothing on the stack to return to.
+;ret
 
 ___intstart endp
 

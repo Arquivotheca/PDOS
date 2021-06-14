8143f01f4d96a9da36c1631d089855059574fc6f
Paul Edwards <mutazilah@gmail.com>
2021-06-14 15:00:44 +1000

    switch to 32-bit build

--- src/compterm.bat
+++ src/compterm.bat
@@ -2,18 +2,18 @@ rem wasmr -zq -zcm -Dmemodel=large support.asm
 rem tcc -O -c -ml -I..\pdpclib terminal.c pos.c bos.c
 rem tlink -x terminal+bos+pos+support,terminal.exe,,..\pdpclib\borland.lib
 
-wasm -zq -zcm -Dmemodel=large support.asm
-wcl -q -w -c -I. -ml -zl -D__MSDOS__ -fpi87 -s -zdp -zu -I..\pdpclib terminal.c bos.c
-wlink File terminal.obj,bos.obj,support.obj Name terminal.exe Form dos Library ..\pdpclib\watcom.lib Option quiet
+rem wasm -zq -zcm -Dmemodel=large support.asm
+rem wcl -q -w -c -I. -ml -zl -D__MSDOS__ -fpi87 -s -zdp -zu -I..\pdpclib terminal.c bos.c
+rem wlink File terminal.obj,bos.obj,support.obj Name terminal.exe Form dos Library ..\pdpclib\watcom.lib Option quiet
 
-rem gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__WIN32__ terminal.c
-rem aswin -o terminal.o terminal.s
-rem del terminal.s
+gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__WIN32__ terminal.c
+aswin -o terminal.o terminal.s
+del terminal.s
 
-rem gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__32BIT__ bos.c
-rem aswin -o bos.o bos.s
-rem del bos.s
+gccwin -O2 -S -fno-common -ansi -I. -I../pdpclib -D__32BIT__ bos.c
+aswin -o bos.o bos.s
+del bos.s
 
-rem aswin -o support.o support.s
+aswin -o support.o support.s
 
-rem ldwin -o terminal.exe ../pdpclib/w32start.o terminal.o bos.o support.o ../pdpclib/msvcrt.a kernel32.a
+ldwin -o terminal.exe ../pdpclib/w32start.o terminal.o bos.o support.o ../pdpclib/msvcrt.a kernel32.a

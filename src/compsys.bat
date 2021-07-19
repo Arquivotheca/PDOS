13ca3aa569ae3b93f3ad22cac4b6784ce6b43cf2
Paul Edwards <mutazilah@gmail.com>
2021-07-20 08:30:52 +1000

    remove verbiage

--- src/compsys.bat
+++ src/compsys.bat
@@ -1,4 +1,4 @@
 rem Use Watcom's version of int86 routine which compensates
 rem for flags not being popped in INT 25H and 26H
 
-wcl -I. -ml -DWATNATIVE sys.c pos.c
+wcl -q -I. -ml -DWATNATIVE sys.c pos.c
